3ca9ad2350ff1f7523603d195535b180b4e29dfd
Sébastien Saunier <sebastien.saunier@applidget.com>
2012-10-01 11:07:52 +0200

    Don't symlink directories

--- install.sh
+++ install.sh
@@ -1,13 +1,15 @@
 for name in *; do
-  target="$HOME/.$name"
-  if [ -e "$target" ]; then
-    if [ ! -L "$target" ]; then
-      echo "WARNING: $target exists but is not a symlink."
-    fi
-  else
-    if [ "$name" != 'install.sh' ] && [ "$name" != 'README.md' ]; then
-      echo "Symlinking to $target"
-      ln -s "$PWD/$name" "$target"
+  if [ ! -d "$name" ]; then
+    target="$HOME/.$name"
+    if [ -e "$target" ]; then
+      if [ ! -L "$target" ]; then
+        echo "WARNING: $target exists but is not a symlink."
+      fi
+    else
+      if [ "$name" != 'install.sh' ] && [ "$name" != 'README.md' ]; then
+        echo "Symlinking to $target"
+        ln -s "$PWD/$name" "$target"
+      fi
     fi
   fi
 done
