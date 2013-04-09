cc0ac99b8697877588729f04d7ab2d33c05431d5
Sébastien Saunier <sebastien.saunier@gmail.com>
2013-04-09 18:12:43 +0200

    Update the way we discriminate files to link to ~

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 for name in *; do
   if [ ! -d "$name" ]; then
     target="$HOME/.$name"
-    if [ "$name" != 'install.sh' ] && [ "$name" != 'README.md' ]; then
+    if [ ! "$name" =~ '\.sh$' ] && [ "$name" != 'README.md' ]; then
       if [ -e "$target" ]; then           # Does the config file already exist?
         if [ ! -L "$target" ]; then       # as a pure file?
           mv "$target" "$target.backup"   # Then backup it
