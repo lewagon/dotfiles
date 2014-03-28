869977063ed3d707a1f98ebffb1cf3773111ff33
Boris Paillard <boris.paillard@gmail.com>
2014-03-28 12:00:27 +0100

    small typo in tests

--- ruby/02-Arrays-Enumerable/05-Debug-Gym/spec/bugged_concatenate_spec.rb
+++ ruby/02-Arrays-Enumerable/05-Debug-Gym/spec/bugged_concatenate_spec.rb
@@ -3,7 +3,7 @@
 require "spec_helper"
 require "bugged_concatenate"
 
-describe "#method" do
+describe "#build_1984_title" do
 
   it "should fix the bugs after reading the error" do
     build_1984_title.must_equal "1984 GEORGE ORWELL"
