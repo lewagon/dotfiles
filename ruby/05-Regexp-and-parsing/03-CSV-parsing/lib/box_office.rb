8e863e4c6574bb0a367cd0935915e3a73e0b1849
Boris Paillard <boris.paillard@gmail.com>
2014-03-31 12:41:58 +0200

    complete exercise

--- ruby/05-Regexp-and-parsing/03-CSV-parsing/lib/box_office.rb
+++ ruby/05-Regexp-and-parsing/03-CSV-parsing/lib/box_office.rb
@@ -3,15 +3,4 @@ require 'csv'
 
 def most_successfull(number, max_year, file_name)
   #TODO: return the number most successfull movies max_year
-  h = csv_to_hash(file_name)
-  h.select{ |k, v| k.to_i < max_year}.sort_by{ |k, v| v[1] }[0..number-1].map{|movie_desc| movie_desc.flatten }
-end
-
-
-def csv_to_hash(file_name)
-	hash = Hash.new { |hash, key| hash[key] = [] }
-	CSV.foreach(file_name, encoding: "ISO8859-1") do |row|
-	hash[row[1]] = [row[0], row[2]]
-	end
-	hash
 end
--- ruby/05-Regexp-and-parsing/03-CSV-parsing/spec/box_office_spec.rb
+++ ruby/05-Regexp-and-parsing/03-CSV-parsing/spec/box_office_spec.rb
@@ -5,30 +5,30 @@ require "box_office"
 
 describe "#most_successfull" do
   
-  let(:movies_list) { most_successfull(3, 1990, File.dirname(__FILE__) + '/../lib/movies.csv') }
+  let(:movies_list) { most_successfull(3, 1975, File.dirname(__FILE__) + '/../lib/movies.csv') }
   
-  it "should return an array" do
+  it "should return an array of movies" do
     movies_list.must_be_instance_of Array
   end
   
-  it "should represent each movie of the list as an array" do
-    movies_list[0].must_be_instance_of Array
+  it "should represent each movie of the list as a hash" do
+    movies_list.all? {|movie| movie.is_a? Hash }.must_equal true
   end
   
-  it "should format each movie exactly as [year, name, box_office]" do
-    movies_list[0].size.must_equal 3
+  it "should represent a movie with at least keys: name, year, earnings" do
+    movies_list.first.keys.all? { |key| [:name, :year, :earnings].include? key }.must_equal true
   end
   
-  it "should keep only movies where (year < max year)" do
-    movies_list.all?{ |movie| movie[0].to_i < 1990}.must_equal true
+  it "should only keep movies where (year < max year)" do
+    movies_list.all?{ |movie| movie[:year] < 1990}.must_equal true
   end
   
-  it "should sort movies depending on box office results" do
-    movies_list.sort_by{ |m| m[0] }.must_equal movies_list
+  it "should pick movies with maximum earnings" do
+    target = [
+            {:name=>"L'exorciste", :year=>1973, :earnings=>204565000}, 
+            {:name=>"Autant en emporte le vent", :year=>1939, :earnings=>198655278}, 
+            {:name=>"Blanche Neige et les sept nains", :year=>1937, :earnings=>184925485}
+          ]
+    movies_list.must_equal target
   end
-  
-  it "should perform the right query" do
-    movies_list.must_equal [["1970", "Airport", "100489150"], ["1980", "Faut s'faire la malle", "101500000"], ["1964", "Mary Poppins", "102300000"]] 
-  end
-
 end
