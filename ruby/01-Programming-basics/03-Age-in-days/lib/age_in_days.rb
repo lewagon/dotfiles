2117aedbb69c7bc96caba3f06bbb7f05e4384057
Olivier Lance <olivier@digitalcuisine.fr>
2014-04-01 15:29:36 +0200

    Added explanation about the "require" directive

--- ruby/01-Programming-basics/03-Age-in-days/lib/age_in_days.rb
+++ ruby/01-Programming-basics/03-Age-in-days/lib/age_in_days.rb
@@ -1,5 +1,8 @@
+# This "require" line loads the contents of the 'date' file from the standard
+# Ruby library, giving you access to the Date class.
 require 'date'
 
+
 def age_in_days(day, month, year)
-  #TODO: return the age expressed in days given the day, month, and year of birth     
+  #TODO: return the age expressed in days given the day, month, and year of birth
 end
