6bee1bdc12eb046aee9b3072cf91c154b5685113
Boris Paillard <boris.paillard@gmail.com>
2014-04-04 18:44:59 +0200

    first version without specs

--- ruby/03-Blocks/03-Beautify-my-code/lib/chain_unchain.rb
+++ ruby/03-Blocks/03-Beautify-my-code/lib/chain_unchain.rb
@@ -16,7 +16,3 @@ def quote_prime_numbers(n)
   
   (1..n).find_all {|i| (2..i-1).select {|k| i % k == 0 }.count == 0 }.map{ |prime_num| "#{prime_num} is prime"}
 end
-
-
-p shuffle_word("nabuchodonosor")
-p quote_prime_numbers(30)
