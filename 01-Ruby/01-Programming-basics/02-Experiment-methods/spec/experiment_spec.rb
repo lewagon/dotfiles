5123e45202694fa947eee022b1a2b6ee927148d2
Sebastien Saunier <seb@saunier.me>
2014-04-07 19:14:14 +0200

    Fix exercise

--- 01-Ruby/01-Programming-basics/02-Experiment-methods/spec/experiment_spec.rb
+++ 01-Ruby/01-Programming-basics/02-Experiment-methods/spec/experiment_spec.rb
@@ -31,7 +31,7 @@ end
 describe "#replace" do
 
   it "should correctly replace the letter in the string" do
-    response = replace("casanova", "a", "o") 
+    response = replace("casanova", "a", "o")
     response.must_equal "cosonovo"
   end
 
@@ -52,17 +52,17 @@ describe "#is_divisible_by_two" do
     response = is_divisible_by_two(6)
     response.must_equal true
   end
-  
+
   it "should return false if number is odd" do
     response = is_divisible_by_two(7)
     response.must_equal false
   end
-  
+
 
 end
 
 describe "#random_subset" do
-  
+
   array = ('a'..'z').to_a
   response = random_subset(('a'..'z').to_a, 4)
 
@@ -70,23 +70,23 @@ describe "#random_subset" do
     response.must_be_instance_of Array
     response.size.must_equal 4 if response.is_a? Array
   end
-  
+
   it "should return random elements" do
     other_response = random_subset(('a'..'z').to_a, 4)
     response.wont_equal other_response
   end
 
   it "should return elements present in the initial array" do
-    (response - array).must_be_empty if response.is_a? Array
+    (response - array).must_be_empty
   end
-  
+
 end
 
 describe "#randomize" do
-  
+
   array = ('a'..'z').to_a
   response = randomize(array)
-  
+
   it "should return random elements" do
     other_response = randomize(array)
     response.wont_equal other_response
@@ -94,18 +94,18 @@ describe "#randomize" do
 
   it "should return random copy of the initial array" do
     response.must_be_instance_of Array
-    response.sort.must_equal array.sort if response.is_a? Array
+    response.sort.must_equal array.sort
   end
-  
+
 end
 
 describe "#ascending_order" do
-      
+
   it "should return the sorted array" do
     array = ('a'..'z').to_a.shuffle
     response = ascending_order(array)
     response.must_equal array.sort
   end
-  
+
 end
 
