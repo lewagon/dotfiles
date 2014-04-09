389668162163e76bbf8fc5edac909d724ab3a57c
Sebastien Saunier <seb@saunier.me>
2014-04-09 17:54:08 +0200

    Fix optional shuffle first spec so that rake does not pass 1 exercise

--- 01-Ruby/02-Arrays-Enumerable/Optional-02-Shuffle/spec/shuffle_spec.rb
+++ 01-Ruby/02-Arrays-Enumerable/Optional-02-Shuffle/spec/shuffle_spec.rb
@@ -11,6 +11,8 @@ describe '#shuffle' do
   it 'sorts an array in a random way' do
     a = [1, 2, 3, 4]
     b = shuffle(a)
+    b.wont_equal nil
+    b.length.must_equal a.length
     a.wont_equal b
   end
 end
