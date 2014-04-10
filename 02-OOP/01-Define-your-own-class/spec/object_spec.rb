e6196fca5a44ecb00fff478af83ae364a1632a6f
Sebastien Saunier <seb@saunier.me>
2014-04-10 19:43:25 +0200

    Uber spec for testing any spec

---
+++ 02-OOP/01-Define-your-own-class/spec/object_spec.rb
@@ -0,0 +1,30 @@
+classes = []
+
+Dir.glob("lib/*.rb").each do |file|
+  filename = file.split("lib/").last
+  class_name = filename.split(".rb").first
+  require filename
+  classes <<  Object.const_get(class_name.capitalize)
+end
+
+require "spec_helper"
+
+describe "lib folder" do
+  it "should have at least one file defining one class" do
+    classes.length.wont_equal 0
+  end
+end
+
+classes.each do |klass|
+
+  describe klass do
+    it "should have an initializer taking at least one argument" do
+      klass.instance_method(:initialize).arity.wont_equal 0
+    end
+
+    it "should have at least one instance method" do
+      klass.instance_methods(false).length.wont_equal 0
+    end
+  end
+
+end
