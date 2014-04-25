b051e47d6dc559bb521df5c14c17a8bc9c97198e
Olivier Lance <olivier@digitalcuisine.fr>
2014-04-25 12:38:42 +0200

    Added specs

--- 03-AR-Database/03-Interacting-with-code/lib/queries.rb
+++ 03-AR-Database/03-Interacting-with-code/lib/queries.rb
@@ -1,7 +1,7 @@
 require 'sqlite3'
 
 # opens the database
-database_path = "db/jukebox.sqlite"
+database_path = File.join(File.dirname(__FILE__), 'db/jukebox.sqlite')
 db = SQLite3::Database.new(database_path)
 
 def number_of_rows(db, table_name)
@@ -9,7 +9,7 @@ def number_of_rows(db, table_name)
 end
 
 def sorted_artists(db)
-  #TODO: return array of artist names sorted alphabetically 
+  #TODO: return array of artist names sorted alphabetically
 end
 
 def love_tracks(db)
@@ -19,9 +19,3 @@ end
 def long_tracks(db, min_length)
   #TODO: return tracks with duration > min_length minutes
 end
-
-puts "There are #{number_of_rows(db,"Artist")} artists"
-puts "There are #{number_of_rows(db,"Track")} tracks"
-puts "There are #{number_of_rows(db,"Album")} albums"
-puts "There are #{number_of_rows(db,"Genre")} genres"
-puts "There are #{number_of_rows(db,"MediaType")} media types"
