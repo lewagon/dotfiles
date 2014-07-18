1a7b26d18dd99ff55f2162cb62acab538aea263a
Sebastien Saunier <seb@saunier.me>
2014-07-18 17:11:16 +0200

    Invert arity

--- 02-OOP/03-Cookbook/01-Cookbook-v0/spec/recipe_factory.rb
+++ 02-OOP/03-Cookbook/01-Cookbook-v0/spec/recipe_factory.rb
@@ -6,14 +6,14 @@ end
 
 class RecipeFactory
   def self.build(name, description)
-    if Recipe.allocate.method(:initialize).arity == 1
+    if Recipe.allocate.method(:initialize).arity == 2
+      Recipe.new(name, description)
+    else
       attributes = {
         name: name,
         description: description
       }
       Recipe.new(attributes)
-    else
-      Recipe.new(name, description)
     end
   end
 end
