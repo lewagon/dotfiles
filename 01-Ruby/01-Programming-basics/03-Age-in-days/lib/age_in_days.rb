dcdd5deacb727da1117c15b3ea8caeb5496e4ab5
Sebastien Saunier <seb@saunier.me>
2014-10-05 21:44:06 +0200

    Rubocop does not prevent rspec from running

--- 01-Ruby/01-Programming-basics/03-Age-in-days/lib/age_in_days.rb
+++ 01-Ruby/01-Programming-basics/03-Age-in-days/lib/age_in_days.rb
@@ -4,5 +4,5 @@ require 'date'
 
 
 def age_in_days(day, month, year)
-  #TODO: return the age expressed in days given the day, month, and year of birth
+  # TODO: return the age expressed in days given the day, month, and year of birth
 end
