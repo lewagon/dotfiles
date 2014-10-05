6020cdfb6efd63362a09c01571b0c848d3f2268d
Sebastien Saunier <seb@saunier.me>
2014-10-05 17:27:18 +0200

    RSpec for 02-experiment methods

--- 01-Ruby/01-Programming-basics/02-Experiment-methods/lib/experiment.rb
+++ 01-Ruby/01-Programming-basics/02-Experiment-methods/lib/experiment.rb
@@ -1,4 +1,4 @@
-# Warning: 
+# Warning:
 # - One line of code for each method
 # - Just look in the doc for the right method of the String, Fixnum, and Array classes !
 
@@ -17,7 +17,7 @@ def replace(initial_string, old_letter, new_letter)
   # example: replace("casanova", "a", "o") => "cosonovo"
 end
 
-def exactly_divide(an_integer, a_divider) 
+def exactly_divide(an_integer, a_divider)
   # TODO: return divison of an_integer by a_divider (computing the floating division, NOT the euclidean one)
   # example: exactly_divide(13, 4) => 3.25
 end
@@ -29,7 +29,7 @@ end
 
 def random_subset(an_array, sample_size)
   # TODO: return a random subset of an_array with size sample_size
-  # example: random_subset(('a'..'z').to_a, 4) => ["u", "q", "l", "t"] 
+  # example: random_subset(('a'..'z').to_a, 4) => ["u", "q", "l", "t"]
 end
 
 def randomize(an_array)
