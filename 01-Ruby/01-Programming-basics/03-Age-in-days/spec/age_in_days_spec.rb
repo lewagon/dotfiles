ac42b8b04db2b5a2db88c3deb9c5aded5358a193
Sebastien Saunier <seb@saunier.me>
2014-10-05 17:37:40 +0200

    Update testing code with RSpec for 03-Age-In-Days

--- 01-Ruby/01-Programming-basics/03-Age-in-days/spec/age_in_days_spec.rb
+++ 01-Ruby/01-Programming-basics/03-Age-in-days/spec/age_in_days_spec.rb
@@ -1,19 +1,18 @@
 # Encoding: utf-8
-require 'spec_helper'
 require 'age_in_days'
 
 describe '#age_in_days' do
 
   it 'should return a Fixnum object' do
     response = age_in_days(1, 1, 2000)
-    response.must_be_instance_of Fixnum
+    expect(response).to be_a Fixnum
   end
 
   it 'should compute the right number of days' do
     target_age = 365 * 25
     birthdate = Date.today - target_age
     response = age_in_days(birthdate.day, birthdate.month, birthdate.year)
-    response.must_equal target_age
+    expect(response).to eq(target_age)
   end
 
 end
