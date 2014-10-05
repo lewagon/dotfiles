ca62d97220145c8dcee5608e07882b78e8aed5e8
Sebastien Saunier <seb@saunier.me>
2014-10-05 17:51:28 +0200

    Redo the what's your name exercice with RSpec and less boilerplate code

--- 01-Ruby/01-Programming-basics/04-What-your-name/lib/interface.rb
+++ 01-Ruby/01-Programming-basics/04-What-your-name/lib/interface.rb
@@ -1,23 +1,8 @@
 require_relative "compute_name"
 
-def ask_and_get(param)
-  #TODO:
-  #- Ask the user to write a value for param
-  #- Get and return the string inputted by the user
-end
+# TODO: ask for the first name
+# TODO: ask for the middle name
+# TODO: ask for the last name
+# TODO: Print a greeting to the user with the full name concatenated,
+#       something like "Hello, Boris Paillard"
 
-def name_from_terminal
-  first_name = ask_and_get("first name")
-  second_name = ask_and_get("middle name")
-  last_name = ask_and_get("last name")
-
-  compute_name(first_name, second_name, last_name)
-end
-
-full_name = name_from_terminal
-
-#TODO: use string concatenation to build a custom message embedding the computed full_name
-
-# custom_message =
-
-puts custom_message
--- 01-Ruby/01-Programming-basics/04-What-your-name/spec/compute_name_spec.rb
+++ 01-Ruby/01-Programming-basics/04-What-your-name/spec/compute_name_spec.rb
@@ -1,15 +1,13 @@
 # Encoding: utf-8
-
-require "spec_helper"
 require "compute_name"
 
 describe "#compute_name" do
   it 'returns a String' do
-    compute_name('', '', '').must_be_instance_of String
+    expect(compute_name('', '', '')).to be_a String
   end
 
   it 'concatenates all parameters together' do
-    compute_name('Jean', 'Michel', 'Saurin').must_equal 'Jean Michel Saurin'
+    expect( compute_name('Jean', 'Michel', 'Saurin') ).to eq('Jean Michel Saurin')
   end
 
 end
