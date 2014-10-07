e066a2e204d20466f2684ef335d9e8db7e62b91c
Sebastien Saunier <seb@saunier.me>
2014-10-07 18:35:28 +0200

    Rspec for wagon sort

--- 01-Ruby/02-Arrays-Enumerable/02-Sorting-the-wagon/spec/wagon_sort_spec.rb
+++ 01-Ruby/02-Arrays-Enumerable/02-Sorting-the-wagon/spec/wagon_sort_spec.rb
@@ -1,16 +1,15 @@
-require "spec_helper"
 require "wagon_sort"
 
 describe "#wagon_sort" do
   it "should return an empty array if given an empty array" do
-    wagon_sort([]).must_equal []
+    expect(wagon_sort([])).to eq []
   end
 
   it "should sort all-caps students" do
-    wagon_sort(["BOB", "ALICE", "CHARLIE"]).must_equal(["ALICE", "BOB", "CHARLIE"])
+    expect(wagon_sort(["BOB", "ALICE", "CHARLIE"])).to eq(["ALICE", "BOB", "CHARLIE"])
   end
 
   it "should be case-insensitive" do
-    wagon_sort(["BOB", "alice", "CHARLIE"]).must_equal(["alice", "BOB", "CHARLIE"])
+    expect(wagon_sort(["BOB", "alice", "CHARLIE"])).to eq(["alice", "BOB", "CHARLIE"])
   end
 end
