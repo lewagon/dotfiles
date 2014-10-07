7ddafb509d87bfc3d4985dfb17901b0bf3f32a04
Sebastien Saunier <seb@saunier.me>
2014-10-07 19:54:08 +0200

    RSpec for optional-01 exercise

--- 01-Ruby/02-Arrays-Enumerable/Optional-01-Roman-numerals/lib/roman_numerals.rb
+++ 01-Ruby/02-Arrays-Enumerable/Optional-01-Roman-numerals/lib/roman_numerals.rb
@@ -1,7 +1,7 @@
 def old_roman_numeral(an_integer)
-  #TODO: translate integer in roman number - old-style way
+  # TODO: translate integer in roman number - old-style way
 end
 
-def roman_numeral(an_integer)
-  #TODO: translate integer in roman number - modern-style way
+def new_roman_numeral(an_integer)
+  # TODO: translate integer in roman number - modern-style way
 end
--- 01-Ruby/02-Arrays-Enumerable/Optional-01-Roman-numerals/spec/roman_numerals_spec.rb
+++ 01-Ruby/02-Arrays-Enumerable/Optional-01-Roman-numerals/spec/roman_numerals_spec.rb
@@ -1,68 +1,67 @@
-require "spec_helper"
 require "roman_numerals"
 
 describe "#old_roman_numeral" do
 
   it "should work for numbers between 1 and 4" do
-    old_roman_numeral(1).must_equal "I"
-    old_roman_numeral(2).must_equal "II"
-    old_roman_numeral(3).must_equal "III"
-    old_roman_numeral(4).must_equal "IIII"
+    expect(old_roman_numeral(1)).to eq "I"
+    expect(old_roman_numeral(2)).to eq "II"
+    expect(old_roman_numeral(3)).to eq "III"
+    expect(old_roman_numeral(4)).to eq "IIII"
   end
 
   it "should work for numbers between 5 and 9" do
-    old_roman_numeral(5).must_equal "V"
-    old_roman_numeral(6).must_equal "VI"
-    old_roman_numeral(7).must_equal "VII"
-    old_roman_numeral(8).must_equal "VIII"
-    old_roman_numeral(9).must_equal "VIIII"
+    expect(old_roman_numeral(5)).to eq "V"
+    expect(old_roman_numeral(6)).to eq "VI"
+    expect(old_roman_numeral(7)).to eq "VII"
+    expect(old_roman_numeral(8)).to eq "VIII"
+    expect(old_roman_numeral(9)).to eq "VIIII"
   end
 
   it "should work for numbers between 10 and 49" do
-    old_roman_numeral(10).must_equal "X"
-    old_roman_numeral(11).must_equal "XI"
-    old_roman_numeral(12).must_equal "XII"
-    old_roman_numeral(13).must_equal "XIII"
-    old_roman_numeral(14).must_equal "XIIII"
-    old_roman_numeral(15).must_equal "XV"
-    old_roman_numeral(19).must_equal "XVIIII"
-    old_roman_numeral(20).must_equal "XX"
-    old_roman_numeral(21).must_equal "XXI"
-    old_roman_numeral(25).must_equal "XXV"
-    old_roman_numeral(29).must_equal "XXVIIII"
-    old_roman_numeral(49).must_equal "XXXXVIIII"
+    expect(old_roman_numeral(10)).to eq "X"
+    expect(old_roman_numeral(11)).to eq "XI"
+    expect(old_roman_numeral(12)).to eq "XII"
+    expect(old_roman_numeral(13)).to eq "XIII"
+    expect(old_roman_numeral(14)).to eq "XIIII"
+    expect(old_roman_numeral(15)).to eq "XV"
+    expect(old_roman_numeral(19)).to eq "XVIIII"
+    expect(old_roman_numeral(20)).to eq "XX"
+    expect(old_roman_numeral(21)).to eq "XXI"
+    expect(old_roman_numeral(25)).to eq "XXV"
+    expect(old_roman_numeral(29)).to eq "XXVIIII"
+    expect(old_roman_numeral(49)).to eq "XXXXVIIII"
   end
 
   it "should work for numbers between 50 and 99" do
-    old_roman_numeral(50).must_equal "L"
-    old_roman_numeral(51).must_equal "LI"
-    old_roman_numeral(99).must_equal "LXXXXVIIII"
+    expect(old_roman_numeral(50)).to eq "L"
+    expect(old_roman_numeral(51)).to eq "LI"
+    expect(old_roman_numeral(99)).to eq "LXXXXVIIII"
   end
 
   it "should work for numbers between 100 and 499" do
-    old_roman_numeral(100).must_equal "C"
-    old_roman_numeral(101).must_equal "CI"
-    old_roman_numeral(149).must_equal "CXXXXVIIII"
-    old_roman_numeral(199).must_equal "CLXXXXVIIII"
-    old_roman_numeral(200).must_equal "CC"
-    old_roman_numeral(499).must_equal "CCCCLXXXXVIIII"
+    expect(old_roman_numeral(100)).to eq "C"
+    expect(old_roman_numeral(101)).to eq "CI"
+    expect(old_roman_numeral(149)).to eq "CXXXXVIIII"
+    expect(old_roman_numeral(199)).to eq "CLXXXXVIIII"
+    expect(old_roman_numeral(200)).to eq "CC"
+    expect(old_roman_numeral(499)).to eq "CCCCLXXXXVIIII"
   end
 
   it "should work for numbers between 500 and 999" do
-    old_roman_numeral(500).must_equal "D"
-    old_roman_numeral(501).must_equal "DI"
-    old_roman_numeral(600).must_equal "DC"
-    old_roman_numeral(700).must_equal "DCC"
-    old_roman_numeral(999).must_equal "DCCCCLXXXXVIIII"
+    expect(old_roman_numeral(500)).to eq "D"
+    expect(old_roman_numeral(501)).to eq "DI"
+    expect(old_roman_numeral(600)).to eq "DC"
+    expect(old_roman_numeral(700)).to eq "DCC"
+    expect(old_roman_numeral(999)).to eq "DCCCCLXXXXVIIII"
   end
 
   it "should work for numbers above 1000" do
-    old_roman_numeral(1000).must_equal "M"
-    old_roman_numeral(1001).must_equal "MI"
-    old_roman_numeral(1100).must_equal "MC"
-    old_roman_numeral(1500).must_equal "MD"
-    old_roman_numeral(2000).must_equal "MM"
-    old_roman_numeral(3000).must_equal "MMM"
+    expect(old_roman_numeral(1000)).to eq "M"
+    expect(old_roman_numeral(1001)).to eq "MI"
+    expect(old_roman_numeral(1100)).to eq "MC"
+    expect(old_roman_numeral(1500)).to eq "MD"
+    expect(old_roman_numeral(2000)).to eq "MM"
+    expect(old_roman_numeral(3000)).to eq "MMM"
   end
 
 end
@@ -70,65 +69,65 @@ end
 describe "#new_roman_numeral" do
 
   it "should work for numbers between 1 and 4" do
-    new_roman_numeral(1).must_equal "I"
-    new_roman_numeral(2).must_equal "II"
-    new_roman_numeral(3).must_equal "III"
-    new_roman_numeral(4).must_equal "IV"
+    expect(new_roman_numeral(1)).to eq "I"
+    expect(new_roman_numeral(2)).to eq "II"
+    expect(new_roman_numeral(3)).to eq "III"
+    expect(new_roman_numeral(4)).to eq "IV"
   end
 
   it "should work for numbers between 5 and 9" do
-    new_roman_numeral(5).must_equal "V"
-    new_roman_numeral(6).must_equal "VI"
-    new_roman_numeral(7).must_equal "VII"
-    new_roman_numeral(8).must_equal "VIII"
-    new_roman_numeral(9).must_equal "IX"
+    expect(new_roman_numeral(5)).to eq "V"
+    expect(new_roman_numeral(6)).to eq "VI"
+    expect(new_roman_numeral(7)).to eq "VII"
+    expect(new_roman_numeral(8)).to eq "VIII"
+    expect(new_roman_numeral(9)).to eq "IX"
   end
 
   it "should work for numbers between 10 and 49" do
-    new_roman_numeral(10).must_equal "X"
-    new_roman_numeral(11).must_equal "XI"
-    new_roman_numeral(12).must_equal "XII"
-    new_roman_numeral(13).must_equal "XIII"
-    new_roman_numeral(14).must_equal "XIV"
-    new_roman_numeral(15).must_equal "XV"
-    new_roman_numeral(19).must_equal "XIX"
-    new_roman_numeral(20).must_equal "XX"
-    new_roman_numeral(21).must_equal "XXI"
-    new_roman_numeral(25).must_equal "XXV"
-    new_roman_numeral(29).must_equal "XXIX"
-    new_roman_numeral(49).must_equal "XLIX"
+    expect(new_roman_numeral(10)).to eq "X"
+    expect(new_roman_numeral(11)).to eq "XI"
+    expect(new_roman_numeral(12)).to eq "XII"
+    expect(new_roman_numeral(13)).to eq "XIII"
+    expect(new_roman_numeral(14)).to eq "XIV"
+    expect(new_roman_numeral(15)).to eq "XV"
+    expect(new_roman_numeral(19)).to eq "XIX"
+    expect(new_roman_numeral(20)).to eq "XX"
+    expect(new_roman_numeral(21)).to eq "XXI"
+    expect(new_roman_numeral(25)).to eq "XXV"
+    expect(new_roman_numeral(29)).to eq "XXIX"
+    expect(new_roman_numeral(49)).to eq "XLIX"
   end
 
   it "should work for numbers between 50 and 99" do
-    new_roman_numeral(50).must_equal "L"
-    new_roman_numeral(51).must_equal "LI"
-    new_roman_numeral(99).must_equal "XCIX"
+    expect(new_roman_numeral(50)).to eq "L"
+    expect(new_roman_numeral(51)).to eq "LI"
+    expect(new_roman_numeral(99)).to eq "XCIX"
   end
 
   it "should work for numbers between 100 and 499" do
-    new_roman_numeral(100).must_equal "C"
-    new_roman_numeral(101).must_equal "CI"
-    new_roman_numeral(149).must_equal "CXLIX"
-    new_roman_numeral(199).must_equal "CXCIX"
-    new_roman_numeral(200).must_equal "CC"
-    new_roman_numeral(499).must_equal "CDXCIX"
+    expect(new_roman_numeral(100)).to eq "C"
+    expect(new_roman_numeral(101)).to eq "CI"
+    expect(new_roman_numeral(149)).to eq "CXLIX"
+    expect(new_roman_numeral(199)).to eq "CXCIX"
+    expect(new_roman_numeral(200)).to eq "CC"
+    expect(new_roman_numeral(499)).to eq "CDXCIX"
   end
 
   it "should work for numbers between 500 and 999" do
-    new_roman_numeral(500).must_equal "D"
-    new_roman_numeral(501).must_equal "DI"
-    new_roman_numeral(600).must_equal "DC"
-    new_roman_numeral(700).must_equal "DCC"
-    new_roman_numeral(999).must_equal "CMXCIX"
+    expect(new_roman_numeral(500)).to eq "D"
+    expect(new_roman_numeral(501)).to eq "DI"
+    expect(new_roman_numeral(600)).to eq "DC"
+    expect(new_roman_numeral(700)).to eq "DCC"
+    expect(new_roman_numeral(999)).to eq "CMXCIX"
   end
 
   it "should work for numbers above 1000" do
-    new_roman_numeral(1000).must_equal "M"
-    new_roman_numeral(1001).must_equal "MI"
-    new_roman_numeral(1100).must_equal "MC"
-    new_roman_numeral(1500).must_equal "MD"
-    new_roman_numeral(2000).must_equal "MM"
-    new_roman_numeral(3000).must_equal "MMM"
+    expect(new_roman_numeral(1000)).to eq "M"
+    expect(new_roman_numeral(1001)).to eq "MI"
+    expect(new_roman_numeral(1100)).to eq "MC"
+    expect(new_roman_numeral(1500)).to eq "MD"
+    expect(new_roman_numeral(2000)).to eq "MM"
+    expect(new_roman_numeral(3000)).to eq "MMM"
   end
 
 end
