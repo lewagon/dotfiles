1498773b860ec7600299d3f7e47473b4e7e71e13
Sebastien Saunier <seb@saunier.me>
2014-10-07 20:14:49 +0200

    Add test for 2 beers

--- 01-Ruby/02-Arrays-Enumerable/Optional-03-Beer-song/spec/beer_song_spec.rb
+++ 01-Ruby/02-Arrays-Enumerable/Optional-03-Beer-song/spec/beer_song_spec.rb
@@ -9,12 +9,20 @@ describe "#beer_song" do
 
       result = o.read
     end
-    result
+    result.chomp
   end
 
-  it "should sing the beer song for one bear!" do
+  it "should sing the beer song for one beer!" do
     result = run_beer_song(1)
     expect(result).to eq "1 bottle of beer on the wall, 1 bottle of beer!
+Take one down, pass it around, no more bottles of beer on the wall!"
+  end
+
+  it "should sing the beer song for two beers" do
+    result = run_beer_song(2)
+    expect(result).to eq "2 bottles of beer on the wall, 2 bottles of beer!
+Take one down, pass it around, 1 bottles of beer on the wall!
+1 bottle of beer on the wall, 1 bottle of beer!
 Take one down, pass it around, no more bottles of beer on the wall!"
   end
 end
