3dbfb344a31313142cabf2af54cf5e5758bda4c0
Sebastien Saunier <seb@saunier.me>
2014-10-07 19:49:18 +0200

    Rspec for PMU

--- 01-Ruby/02-Arrays-Enumerable/05-Destructive-Methods/lib/pmu.rb
+++ 01-Ruby/02-Arrays-Enumerable/05-Destructive-Methods/lib/pmu.rb
@@ -1,5 +1,5 @@
 def pmu_format!(race_array)
-  #TODO: modify the given array so that it is PMU-consistent
+  # TODO: modify the given array so that it is PMU-consistent. This method should return nil.
 end
 
 
