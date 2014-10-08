132443dc27448b36ad52ca677cd22f7ad484ea47
Sebastien Saunier <seb@saunier.me>
2014-10-08 11:49:50 +0200

    Migrate last exercise to rspec

--- 01-Ruby/03-Blocks/Optional-03-Storing-blocks-in-Proc/spec/love_message_spec.rb
+++ 01-Ruby/03-Blocks/Optional-03-Storing-blocks-in-Proc/spec/love_message_spec.rb
@@ -1,42 +1,41 @@
 # Encoding: utf-8
 
-require "spec_helper"
 require "love_message"
 
 describe "#tell" do
 
   it "should tell something to someone" do
     message = tell("tromblard") { "j'ai rencard avec nanar" }
-    message.must_equal "tromblard, j'ai rencard avec nanar !"
+    expect(message).to eq "tromblard, j'ai rencard avec nanar !"
   end
 
 end
 
 describe "#tell_mum" do
-  
+
   it "should tell something to your mum'" do
     message = tell_mum {"I love you"}
-    message.must_equal "mum, I love you !"
+    expect(message).to eq "mum, I love you !"
   end
 
 end
 
 describe "#tell_with_proc" do
-  
+
   it "should tell something to someone passing a Proc" do
     block = Proc.new {"j'ai rencard avec nanar"}
-    message = tell_with_proc("tromblard", block) 
-    message.must_equal "tromblard, j'ai rencard avec nanar !"
+    message = tell_with_proc("tromblard", block)
+    expect(message).to eq "tromblard, j'ai rencard avec nanar !"
   end
 
 end
 
 describe "#tell_mum_with_proc" do
-  
+
   it "should tell something to your mum passing a Proc" do
     love_block = Proc.new {"I love you"}
     message = tell_mum_with_proc(love_block)
-    message.must_equal "mum, I love you !"
+    expect(message).to eq "mum, I love you !"
   end
 
 end
