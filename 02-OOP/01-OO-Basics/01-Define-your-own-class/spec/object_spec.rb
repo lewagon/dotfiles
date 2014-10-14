fd4e53ee5c281ce33500735eb86fa482bbc1faf5
Sebastien Saunier <seb@saunier.me>
2014-10-14 14:51:14 +0200

    Rubocop for first OO exercise

--- 02-OOP/01-OO-Basics/01-Define-your-own-class/spec/object_spec.rb
+++ 02-OOP/01-OO-Basics/01-Define-your-own-class/spec/object_spec.rb
@@ -1,7 +1,5 @@
 classes = []
 
-require "spec_helper"
-
 Dir.glob("lib/*.rb").each do |file|
   filename = file.split("lib/").last
   class_name = filename.split(".rb").first
@@ -11,7 +9,7 @@ end
 
 describe "lib folder" do
   it "should have at least one file defining one class" do
-    classes.length.wont_equal 0
+    expect(classes.length).not_to eq 0
   end
 end
 
@@ -19,11 +17,11 @@ classes.each do |klass|
 
   describe klass do
     it "should have an initializer taking at least one argument" do
-      klass.instance_method(:initialize).arity.wont_equal 0
+      (klass.instance_method(:initialize).arity).not_to eq 0
     end
 
     it "should have at least one instance method" do
-      klass.instance_methods(false).length.wont_equal 0
+      (klass.instance_methods(false).length).not_to eq 0
     end
   end
 
