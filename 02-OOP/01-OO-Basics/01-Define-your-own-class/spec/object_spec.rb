9d61b15d3c063513d54186c21faed5dfb4611be0
Sebastien Saunier <seb@saunier.me>
2014-10-14 15:07:39 +0200

    Fix specs

--- 02-OOP/01-OO-Basics/01-Define-your-own-class/spec/object_spec.rb
+++ 02-OOP/01-OO-Basics/01-Define-your-own-class/spec/object_spec.rb
@@ -4,7 +4,7 @@ Dir.glob("lib/*.rb").each do |file|
   filename = file.split("lib/").last
   class_name = filename.split(".rb").first
   require filename
-  classes <<  Object.const_get(camelize(class_name))
+  classes <<  Object.const_get(class_name.split('_').collect(&:capitalize).join)
 end
 
 describe "lib folder" do
@@ -17,11 +17,11 @@ classes.each do |klass|
 
   describe klass do
     it "should have an initializer taking at least one argument" do
-      (klass.instance_method(:initialize).arity).not_to eq 0
+      expect(klass.instance_method(:initialize).arity).not_to eq 0
     end
 
     it "should have at least one instance method" do
-      (klass.instance_methods(false).length).not_to eq 0
+      expect(klass.instance_methods(false).length).not_to eq 0
     end
   end
 
