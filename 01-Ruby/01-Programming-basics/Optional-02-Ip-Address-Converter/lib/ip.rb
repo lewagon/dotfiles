cc794bd4481e3844bafe4981b3a0f04f76a7f4c4
Sebastien Saunier <seb@saunier.me>
2014-10-22 10:01:32 +0200

    IP to Number optional exercise

---
+++ 01-Ruby/01-Programming-basics/Optional-02-Ip-Address-Converter/lib/ip.rb
@@ -0,0 +1,7 @@
+def ip_to_num(ip_address)
+  # TODO: return the number version of the `ip_address` string
+end
+
+def num_to_ip(number)
+  # TODO: return the string IP address from the `number`
+end
