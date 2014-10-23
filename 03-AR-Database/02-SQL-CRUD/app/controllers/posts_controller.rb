9fcb369e52c460ae40086500a217871c4f31080d
Sebastien Saunier <seb@saunier.me>
2014-10-24 00:07:11 +0200

    CRUD example completely refactored. TODO: Readme

---
+++ 03-AR-Database/02-SQL-CRUD/app/controllers/posts_controller.rb
@@ -0,0 +1,25 @@
+class PostsController
+  def initialize
+    @view = PostView.new
+  end
+
+  def index
+    # TODO: implement listing all posts
+  end
+
+  def create
+    # TODO: implement creating a new post
+  end
+
+  def update
+    # TODO: implement updating an existing post
+  end
+
+  def destroy
+    # TODO: implement destroying a post
+  end
+
+  def upvote
+    # TODO: implement upvoting a post
+  end
+end
