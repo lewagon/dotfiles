e6118863640011b0ce2fc933387aeff211cba011
Sebastien Saunier <seb@saunier.me>
2014-10-25 17:42:34 +0200

    s/100/10

--- 03-AR-Database/03-ActiveRecord-Basics/04-Seed-API/spec/seeds_spec.rb
+++ 03-AR-Database/03-ActiveRecord-Basics/04-Seed-API/spec/seeds_spec.rb
@@ -9,8 +9,8 @@ describe "Seed" do
 
   after(:each) { drop_db }
 
-  it "should insert 100 posts in the database from the Hacker News API" do
+  it "should insert 10 posts in the database from the Hacker News API" do
     require "#{__dir__}/../db/seeds.rb"
-    expect(Post.count).to eq 100
+    expect(Post.count).to eq 10
   end
 end
