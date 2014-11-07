d724d611395b92b9cc743b76ca630e3153a62ee9
Augustin Riedinger <augustin.riedinger@gmail.com>
2014-11-07 10:24:59 +0100

    automated tests for optional 1 04 hashes

--- 01-Ruby/04-Hash-Symbols/Optional-01-Roman-to-integer/lib/roman_to_integer.rb
+++ 01-Ruby/04-Hash-Symbols/Optional-01-Roman-to-integer/lib/roman_to_integer.rb
@@ -1,9 +1,5 @@
+# encoding: UTF-8
+
 def roman_to_integer(roman_string)
-  #TODO: translate roman string to integer
+  # TODO: translate roman string to integer
 end
-
-# testing your program
-puts roman_to_integer('IV') == 4 # => true
-puts roman_to_integer('XII') == 12 # => true
-puts roman_to_integer('XIX') == 19 # => true
-
