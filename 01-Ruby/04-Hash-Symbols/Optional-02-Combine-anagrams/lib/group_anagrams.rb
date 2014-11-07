879e061f48ed8017c52bd54ce3a05f8db025a0a0
Augustin Riedinger <augustin.riedinger@gmail.com>
2014-11-07 10:34:13 +0100

    added tests for 04 hash symbols optional 2 combine anagrams

--- 01-Ruby/04-Hash-Symbols/Optional-02-Combine-anagrams/lib/group_anagrams.rb
+++ 01-Ruby/04-Hash-Symbols/Optional-02-Combine-anagrams/lib/group_anagrams.rb
@@ -1,8 +1,5 @@
+# encoding: UTF-8
+
 def group_anagrams(words)
-  #TODO: group anagrams
+  # TODO: group anagrams
 end
-
-# input:
-p group_anagrams( ['cars', 'for', 'potatoes', 'racs', 'four','scar', 'creams', 'scream'] )
-
-# =>  [["cars", "racs", "scar"], ["four"], ["for"], ["potatoes"], ["creams", "scream"]]
---
+++ 01-Ruby/04-Hash-Symbols/Optional-02-Combine-anagrams/spec/group_anagrams_spec.rb
@@ -0,0 +1,16 @@
+require "group_anagrams"
+
+describe "#group_anagrams" do
+  it "should return an empty array when empty array taken" do
+    expect(group_anagrams([])).to eq([])
+  end
+  
+  it "should return an array of arrays" do
+    expect(group_anagrams(['cars', 'for', 'potatoes', 'racs', 'four','scar', 'creams', 'scream']).reduce(true){ |bool, el| bool && el.class.name == "Array"}).to eq(true)
+  end
+  
+  it "should group anagrams together" do
+    expect(group_anagrams(['cars', 'for', 'potatoes', 'racs', 'four','scar', 'creams', 'scream'])).to eq([["cars", "racs", "scar"], ["four"], ["for"], ["potatoes"], ["creams", "scream"]])
+  end
+
+end
