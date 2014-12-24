46c1171f38fe8b01a1ace99349169d87f52a0f6e
Sebastien Saunier <seb@saunier.me>
2014-12-24 16:26:02 +0100

    Scramble box office to force students to sort

--- 01-Ruby/06-Parsing/01-CSV-Parsing/spec/box_office_spec.rb
+++ 01-Ruby/06-Parsing/01-CSV-Parsing/spec/box_office_spec.rb
@@ -3,30 +3,45 @@ require "box_office"
 
 describe "#most_successfull" do
 
-  let(:movies_list) { most_successfull(3, 1975, File.dirname(__FILE__) + '/../lib/movies.csv') }
+  context "Most successful movies before 1975" do
+    let(:movies_list) { most_successfull(3, 1975, File.dirname(__FILE__) + '/../lib/movies.csv') }
 
-  it "should return an array of movies" do
-    expect(movies_list).to be_a Array
+    it "should return an array of movies" do
+      expect(movies_list).to be_a Array
+    end
+
+    it "should represent each movie of the list as a hash" do
+      expect(movies_list.all? {|movie| movie.is_a? Hash }).to eq true
+    end
+
+    it "should represent a movie with keys: name, year, earnings" do
+      expect(movies_list.first.keys.all? { |key| [:name, :year, :earnings].include? key }).to eq true
+    end
+
+    it "should only keep movies where (year < max year)" do
+      expect(movies_list.all?{ |movie| movie[:year] < 1975}).to eq true
+    end
+
+    it "should pick movies with maximum earnings" do
+      target = [
+              { name: "The Exorcist", year: 1973, earnings: 204565000 },
+              { name: "Gone with the Wind", year: 1939, earnings: 198655278 },
+              { name: "Snow White and the Seven Dwarfs", year: 1937, earnings: 184925485 }
+            ]
+      expect(movies_list).to eq target
+    end
   end
 
-  it "should represent each movie of the list as a hash" do
-    expect(movies_list.all? {|movie| movie.is_a? Hash }).to eq true
-  end
+  context "Most successful movies ever" do
+    let(:movies_list) { most_successfull(3, 2100, File.dirname(__FILE__) + '/../lib/movies.csv') }
 
-  it "should represent a movie with keys: name, year, earnings" do
-    expect(movies_list.first.keys.all? { |key| [:name, :year, :earnings].include? key }).to eq true
-  end
-
-  it "should only keep movies where (year < max year)" do
-    expect(movies_list.all?{ |movie| movie[:year] < 1975}).to eq true
-  end
-
-  it "should pick movies with maximum earnings" do
-    target = [
-            { name: "Avatar", year: 2009, earnings: 760505847 },
-            { name: "Titanic", year: 1997, earnings: 658672302 },
-            { name: "The Avengers", year: 2012, earnings: 623279547 }
-          ]
-    expect(movies_list).to eq target
+    it "should be Avatar, Titanic and The Avengers" do
+      target = [
+              { name: "Avatar", year: 2009, earnings: 760505847 },
+              { name: "Titanic", year: 1997, earnings: 658672302 },
+              { name: "The Avengers", year: 2012, earnings: 623279547 }
+            ]
+      expect(movies_list).to eq target
+    end
   end
 end
