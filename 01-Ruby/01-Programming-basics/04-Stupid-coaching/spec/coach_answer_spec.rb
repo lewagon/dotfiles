cde2b8b684929fb139719efadf9a8d882833f27c
Cecile Veneziani <zora@patapouf.org>
2015-01-07 13:18:31 +0100

    Update specs of 01-01-04 Stupid Coaching
     - Add missing specs for coach_answer_enhanced method

--- 01-Ruby/01-Programming-basics/04-Stupid-coaching/spec/coach_answer_spec.rb
+++ 01-Ruby/01-Programming-basics/04-Stupid-coaching/spec/coach_answer_spec.rb
@@ -2,7 +2,6 @@
 require "coach_answer"
 
 describe "The method coach_answer" do
-
   it 'returns a String' do
     expect( coach_answer("Hello Coach !") ).to be_a String
   end
@@ -20,18 +19,32 @@ describe "The method coach_answer" do
   end
 end
 
-
 describe "The method coach_answer_enhanced" do
+  it 'returns a String' do
+    expect( coach_answer_enhanced("Hello Coach !") ).to be_a String
+  end
 
-  it 'prepends a motivation statement and answers back he does not care when you tell him something' do
+  it 'answers back he does not care when you tell him something and does not shout' do
+    expect( coach_answer("I would love to eat some pizza!") ).to eq "I don't care, get dressed and go to work!"
+  end
+
+  it 'answers back that\'s a silly question when you ask him something and does not shout' do
+    expect( coach_answer("Can I eat some pizza?") ).to eq "Silly question, get dressed and go to work!"
+  end
+
+  it 'does not answer back when you tell him you are going to work and does not shout' do
+    expect( coach_answer( "I am going to work right now!" ) ).to eq ""
+  end
+
+  it 'prepends a motivation statement and answers back he does not care when you tell him something and you shout' do
     expect( coach_answer_enhanced("I WOULD LOVE SOME PIZZA!") ).to eq "I can feel your motivation! I don't care, get dressed and go to work!"
   end
 
-  it 'prepends a motivation statement and answers back that\'s a silly question when you ask him something' do
+  it 'prepends a motivation statement and answers back that\'s a silly question when you ask him something and you shout' do
     expect( coach_answer_enhanced("CAN I EAT SOME PIZZA?") ).to eq "I can feel your motivation! Silly question, get dressed and go to work!"
   end
 
-  it 'does not answer back when you tell him you are going to work' do
+  it 'does not answer back when you tell him you are going to work and you shout' do
     expect( coach_answer_enhanced( "I AM GOING TO WORK RIGHT NOW!" ) ).to eq ""
   end
 end
