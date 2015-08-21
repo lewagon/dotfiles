b04196cd4d220e588fd43c4ea410db006dd5b8b7
Sebastien Saunier <seb@saunier.me>
2015-08-21 16:19:24 +0200

    Add space to spec

--- 01-Ruby/01-Programming-basics/01-Experiment-methods/spec/experiment_spec.rb
+++ 01-Ruby/01-Programming-basics/01-Experiment-methods/spec/experiment_spec.rb
@@ -6,7 +6,7 @@ describe "#get_rid_of_surrounding_whitespaces" do
 
   it "should get rid of both leading and trailing whitespaces" do
     expect( get_rid_of_surrounding_whitespaces("  hey yo  ") ).to eq("hey yo")
-    expect( get_rid_of_surrounding_whitespaces("nice try!") ).to eq("nice try!")
+    expect( get_rid_of_surrounding_whitespaces(" nice try!") ).to eq("nice try!")
   end
 
 end
