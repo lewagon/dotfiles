011c2e25f12d64ea883f7b52c35bb99c22ffc02a
Cecile Veneziani <contact@cecilitse.org>
2015-09-29 16:32:54 +0200

    Change default value of votes

--- 03-AR-Database/04-ActiveRecord-Advanced/03-Sinatra/db/migrate/20141025152200_init_world.rb
+++ 03-AR-Database/04-ActiveRecord-Advanced/03-Sinatra/db/migrate/20141025152200_init_world.rb
@@ -3,14 +3,17 @@ class InitWorld < ActiveRecord::Migration
     create_table :users do |t|
       t.string :username
       t.string :email
+
       t.timestamps null: false
     end
 
     create_table :posts do |t|
-      t.string :name
-      t.string :url
-      t.integer :votes
-      t.references :user
+      t.string      :name
+      t.string      :url
+      t.integer     :votes, default: 0
+
+      t.references  :user, index: true, foreign_key: true
+
       t.timestamps null: false
     end
   end
