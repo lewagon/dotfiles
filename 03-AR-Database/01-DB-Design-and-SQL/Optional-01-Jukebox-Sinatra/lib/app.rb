db85643090f446bf47984f75fd707cd751b929bb
Sebastien Saunier <sebastien.saunier@gmail.com>
2016-01-21 13:36:03 +0100

    Rakefile for exercise

--- 03-AR-Database/01-DB-Design-and-SQL/Optional-01-Jukebox-Sinatra/lib/app.rb
+++ 03-AR-Database/01-DB-Design-and-SQL/Optional-01-Jukebox-Sinatra/lib/app.rb
@@ -6,11 +6,10 @@ DB = SQLite3::Database.new(File.join(File.dirname(__FILE__), 'db/jukebox.sqlite'
 
 get "/" do
   # TODO: Gather all artists to be displayed on home page
-
-  erb :home  # Will render views/home.erb file (embedded in layout.erb)
+  erb :home # Will render views/home.erb file (embedded in layout.erb)
 end
 
-# TODO:
+# Then:
 # 1. Create an artist page with all the albums. Display genres as well
 # 2. Create an album pages with all the tracks
 # 3. Create a track page, and embed a Youtube video (you might need to hit Youtube API)
