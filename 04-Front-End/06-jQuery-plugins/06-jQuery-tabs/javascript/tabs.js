a2e854d59ed321628a5db03569eebe97775e3c63
Boris Paillard <boris.paillard@gmail.com>
2016-02-01 15:51:30 +0100

    adding jQuery tabs challenge

---
+++ 04-Front-End/06-jQuery-plugins/06-jQuery-tabs/javascript/tabs.js
@@ -0,0 +1,11 @@
+$(function(){
+
+  $(".tab").on("click", function(e){
+    // Change active tab
+
+    // Hide all tab-content (use class="hidden")
+
+    // Show target tab-content (use class="hidden")
+  });
+
+});
