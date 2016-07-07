dc1265d9d0d581d26917d1f62f6fb24f0e937345
Boris Paillard <boris.paillard@gmail.com>
2016-07-07 11:58:02 -0300

    much shorter spec for timer block challenge

--- 01-Ruby/03-Iterators-Blocks/02-About-blocks/spec/block_gym_spec.rb
+++ 01-Ruby/03-Iterators-Blocks/02-About-blocks/spec/block_gym_spec.rb
@@ -40,7 +40,7 @@ describe "#timer_for" do
 
   it "more complex blocks should take more time to execute (in seconds)" do
     long_block_time = timer_for do
-      (1..100).each { |i| (1..100000).to_a.shuffle.sort }
+      (1..10).each { |i| (1..10000).to_a.shuffle.sort }
     end
     expect(block_time < long_block_time).to eq true
   end
