41afa69038d3c11090bbd9e0199e6cb366cf0ffd
Nicolas Filzi <nfilzi.webservices@gmail.com>
2016-07-13 21:41:36 +0200

    Change wording for the tierce exercise
    English speaking people always have trouble understanding what tierce means

---
+++ 02-OOP/02-OO-Advanced/03-About-self/spec/winning_combination_spec.rb
@@ -0,0 +1,9 @@
+# Encoding: utf-8
+
+require "winning_combination"
+
+describe "#winning_combination?" do
+  it "should get the winning combination" do
+    expect(winning_combination?).to eq true
+  end
+end
