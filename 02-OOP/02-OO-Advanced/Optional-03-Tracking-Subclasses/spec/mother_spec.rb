65b3a45ecbbd307767209328a6f60f2fbd6649c6
Edward Schults <edward.schults@gmail.com>
2016-09-07 14:47:14 +0200

    updated mother_spec fixes #221 & #222

--- 02-OOP/02-OO-Advanced/Optional-03-Tracking-Subclasses/spec/mother_spec.rb
+++ 02-OOP/02-OO-Advanced/Optional-03-Tracking-Subclasses/spec/mother_spec.rb
@@ -24,17 +24,31 @@ class Son < Mother
   end
 end
 
+class RandomClass < Mother
+  @phoned = false
+
+  class << self
+    attr_accessor :phoned
+  end
+
+  def self.phone
+    self.phoned = true
+  end
+end
+
 describe Mother do
   before(:each) do
     Daughter.phoned = false
     Son.phoned = false
+    RandomClass.phoned = false
   end
 
   describe "#phone_kids" do
     it "should phone kids" do
       Mother.phone_kids
       expect(Son.phoned).to eq true
-      expect(Daughter.phone).to eq true
+      expect(Daughter.phoned).to eq true
+      expect(RandomClass.phoned).to eq true
     end
   end
 end
