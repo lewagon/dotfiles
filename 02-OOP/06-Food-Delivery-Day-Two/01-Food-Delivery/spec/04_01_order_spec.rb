d304896b8d9dde4b2cfb5ec649d141e27d4853f5
Arthur Littmann <arthur.littmann@gmail.com>
2016-10-19 18:19:11 +0100

    small spelling mistake in string
    recipe should be "order"

--- 02-OOP/06-Food-Delivery-Day-Two/01-Food-Delivery/spec/04_01_order_spec.rb
+++ 02-OOP/06-Food-Delivery-Day-Two/01-Food-Delivery/spec/04_01_order_spec.rb
@@ -67,7 +67,7 @@ describe "Order" do
   end
 
   describe "#deliver!" do
-    it "should mark a recipe as delievered" do
+    it "should mark a order as delievered" do
       order = Order.new
       expect(order.delivered?).to be false
       order.deliver!
