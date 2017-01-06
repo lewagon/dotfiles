be126f6c9a790ad6a115fa69860aa4ea43585809
Sebastien Saunier <seb@saunier.me>
2017-01-06 15:24:45 +0100

    Upgrade anagram spec so that it does not take order into account

--- 01-Ruby/04-Hash-Symbols/Optional-02-Combine-anagrams/spec/group_anagrams_spec.rb
+++ 01-Ruby/04-Hash-Symbols/Optional-02-Combine-anagrams/spec/group_anagrams_spec.rb
@@ -6,14 +6,26 @@ describe "#group_anagrams" do
   end
 
   it "should return an array of arrays" do
-    expect(group_anagrams(['cars', 'for', 'potatoes', 'racs', 'four','scar', 'creams', 'scream']).reduce(true){ |bool, el| bool && el.class.name == "Array"}).to eq(true)
+    expect(group_anagrams(['cars', 'for', 'potatoes', 'racs', 'four','scar', 'creams', 'scream']). \
+      reduce(true) { |bool, el| bool && el.class.name == "Array"}).to eq(true)
   end
 
-  it "should group anagrams together, sorted lexicographically" do
-    output = group_anagrams(['cars', 'for', 'potatoes', 'racs', 'four','scar', 'creams', 'scream'])
-    ouput = output.nil? ? nil : output.sort
-    expect(output).to \
-      eq([["cars", "racs", "scar"], ["creams", "scream"], ["for"], ["four"], ["potatoes"]])
+  it "should group anagrams together" do
+    output = group_anagrams(['cars', 'for', 'potatoes', 'racs', 'four', 'scar', 'creams', 'scream'])
+    cars_group = output.find { |group| group.include?("cars") }
+    creams_group = output.find { |group| group.include?("creams") }
+    potatoes_group = output.find { |group| group.include?("potatoes") }
+    for_group = output.find { |group| group.include?("for") }
+    four_group = output.find { |group| group.include?("four") }
+
+    expect(cars_group).to include("racs")
+    expect(cars_group).to include("scar")
+
+    expect(creams_group).to include("scream")
+
+    expect(potatoes_group.size).to eq(1)
+    expect(for_group.size).to eq(1)
+    expect(four_group.size).to eq(1)
   end
 
 end
