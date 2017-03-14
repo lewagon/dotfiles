8211d08f82093bf5ce4a743033b5e972bfb1d2a4
Sebastien Saunier <seb@saunier.me>
2017-03-14 11:12:57 +0100

    Method should not puts but return. Thanks @Eschults

--- 02-OOP/01-OO-Basics/02-Getters-and-Setters/lib/buying_scenario.rb
+++ 02-OOP/01-OO-Basics/02-Getters-and-Setters/lib/buying_scenario.rb
@@ -1,34 +1,34 @@
 require_relative "vending_machine"
 
 def display(vending_machine)
-  print "---> Amount: #{vending_machine.amount_cents / 100}€"
-  print " - 1 Snack = #{vending_machine.snack_price_cents / 100}€"
-  puts  " - Stock: #{vending_machine.snacks}\n\n"
+  return "---> Amount: #{vending_machine.amount_cents / 100}€" \
+    " - 1 Snack = #{vending_machine.snack_price_cents / 100}€" \
+    " - Stock: #{vending_machine.snacks}"
 end
 
 # We instantiate a new vending machine selling 2.5 euros snacks. In the beginning
 # this machine has a stock of 10 snacks.
 vending_machine = VendingMachine.new(250, 10)
 puts "Vending machine ready!"
-display(vending_machine)
+puts display(vending_machine)
 
 # A client comes and insert some coins
 puts "Inserting 2€"
 vending_machine.insert_coin(200)
-display(vending_machine)
+puts display(vending_machine)
 
 3.times do
   puts "Inserting 0.2€"
   vending_machine.insert_coin(20)
-  display(vending_machine)
+  puts display(vending_machine)
 end
 
 # Then the client pushes the Buy snack button
 puts "Pushing 'Buy Snack' button"
 vending_machine.buy_snack
-display(vending_machine)
+puts display(vending_machine)
 
 # The nthe client pushes the Buy snac button again (but forgot to insert some coins!)
 puts "Pushing 'Buy Snack' button"
 vending_machine.buy_snack
-display(vending_machine)
+puts display(vending_machine)
--- 02-OOP/01-OO-Basics/02-Getters-and-Setters/lib/refilling_scenario.rb
+++ 02-OOP/01-OO-Basics/02-Getters-and-Setters/lib/refilling_scenario.rb
@@ -1,16 +1,18 @@
 require_relative "vending_machine"
 
 def display(vending_machine)
-  puts "---> Amount: #{vending_machine.amount_cents / 100}€ - Stock: #{vending_machine.snacks}\n\n"
+  return "---> Amount: #{vending_machine.amount_cents / 100}€" \
+    " - 1 Snack = #{vending_machine.snack_price_cents / 100}€" \
+    " - Stock: #{vending_machine.snacks}"
 end
 
 # We instantiate an **empty** vending machine selling 2.5 euros snacks.
 vending_machine = VendingMachine.new(250, 0)
 puts "Vending machine ready!"
-display(vending_machine)
+puts display(vending_machine)
 
 # A technician comes and refill the vendinc machine with 20 snacks.
 
 puts "Technician filling machine with 20 snacks"
 vending_machine.snacks = vending_machine.snacks + 20
-display(vending_machine)
+puts display(vending_machine)
