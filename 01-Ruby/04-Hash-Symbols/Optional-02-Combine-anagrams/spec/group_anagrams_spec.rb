56b80e9fcfa0470f011486370182ad283052e7f9
Alex Benoit <alexander.benoit@outlook.com>
2017-04-03 13:41:08 +0100

    takes into account the caps as described in readme

--- 01-Ruby/04-Hash-Symbols/Optional-02-Combine-anagrams/spec/group_anagrams_spec.rb
+++ 01-Ruby/04-Hash-Symbols/Optional-02-Combine-anagrams/spec/group_anagrams_spec.rb
@@ -6,22 +6,23 @@ describe "#group_anagrams" do
   end
 
   it "should return an array of arrays" do
-    expect(group_anagrams(['cars', 'for', 'potatoes', 'racs', 'four','scar', 'creams', 'scream']). \
-      reduce(true) { |bool, el| bool && el.class.name == "Array"}).to eq(true)
+    words = ['cars', 'for', 'POTATOES', 'racs', 'four', 'scar', 'creams', 'SCREAM']
+    expect(group_anagrams(words).reduce(true) { |bool, el| bool && el.class.name == "Array"}).to eq(true)
   end
 
   it "should group anagrams together" do
-    output = group_anagrams(['cars', 'for', 'potatoes', 'racs', 'four', 'scar', 'creams', 'scream'])
+    words = ['cars', 'for', 'POTATOES', 'racs', 'four', 'scar', 'creams', 'SCREAM']
+    output = group_anagrams(words)
     cars_group = output.find { |group| group.include?("cars") }
     creams_group = output.find { |group| group.include?("creams") }
-    potatoes_group = output.find { |group| group.include?("potatoes") }
+    potatoes_group = output.find { |group| group.include?("POTATOES") }
     for_group = output.find { |group| group.include?("for") }
     four_group = output.find { |group| group.include?("four") }
 
     expect(cars_group).to include("racs")
     expect(cars_group).to include("scar")
 
-    expect(creams_group).to include("scream")
+    expect(creams_group).to include("SCREAM")
 
     expect(potatoes_group.size).to eq(1)
     expect(for_group.size).to eq(1)
