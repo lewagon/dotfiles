b2a818a003913dfacb88cc532a92860bca73ec96
Alex Benoit <alexander.benoit@outlook.com>
2017-04-16 18:54:29 +0100

    testing for more complex stuff in rake roman numerals

--- 01-Ruby/04-Hash-Symbols/Optional-01-Roman-to-integer/spec/roman_to_integer_spec.rb
+++ 01-Ruby/04-Hash-Symbols/Optional-01-Roman-to-integer/spec/roman_to_integer_spec.rb
@@ -1,15 +1,16 @@
 require "roman_to_integer"
 
 describe "#roman_to_integer" do
-  
+
   it "should work for several values" do
-    expect(roman_to_integer('IV')).to eq(4) 
-    expect(roman_to_integer('XII')).to eq(12) 
-    expect(roman_to_integer('XIX')).to eq(19) 
-    expect(roman_to_integer('XLIX')).to eq(49) 
-    expect(roman_to_integer('LXXVIII')).to eq(78) 
-    expect(roman_to_integer('LXXIX')).to eq(79) 
-    expect(roman_to_integer('CI')).to eq(101)  
+    expect(roman_to_integer('IV')).to eq(4)
+    expect(roman_to_integer('XII')).to eq(12)
+    expect(roman_to_integer('XIX')).to eq(19)
+    expect(roman_to_integer('XLIX')).to eq(49)
+    expect(roman_to_integer('LXXVIII')).to eq(78)
+    expect(roman_to_integer('LXXIX')).to eq(79)
+    expect(roman_to_integer('CI')).to eq(101)
+    expect(roman_to_integer('MCMLXXXIX')).to eq(1989)
   end
 
 end
