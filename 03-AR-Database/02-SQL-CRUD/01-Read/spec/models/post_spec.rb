158ea02bae40554cb5ed0d6f14379ac8fce99042
Edward Schults <edward.schults@gmail.com>
2017-04-28 17:25:32 +0200

    Revert "enhanced sql injection test"
    This reverts commit f8e559f57fa5877f2f5f99aff0fd16ffdfbfe241.

--- 03-AR-Database/02-SQL-CRUD/01-Read/spec/models/post_spec.rb
+++ 03-AR-Database/02-SQL-CRUD/01-Read/spec/models/post_spec.rb
@@ -60,9 +60,10 @@ describe Post do
     end
 
     it "should resist SQL injections" do
-      id = "' OR 1=1 --"
-      post = Post.find(id)  # Inject SQL to fetch all posts...
-      expect(post.title).to eq 'I am the post 2'
+      id = '(DROP TABLE IF EXISTS `posts`;)'
+      post = Post.find(id)  # Inject SQL to delete the posts table...
+      expect { Post.find(1) }.not_to raise_error
+      expect(Post.find(1).title).to eq 'Hello world'
     end
   end
 
