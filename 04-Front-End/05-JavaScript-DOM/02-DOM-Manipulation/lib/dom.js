115fd4f83539562f15e86c4de2a5d77a65c6b47a
Sébastien Saunier <seb@saunier.me>
2017-07-31 23:25:23 +0200

    DOM Manipulation exercise

---
+++ 04-Front-End/05-JavaScript-DOM/02-DOM-Manipulation/lib/dom.js
@@ -0,0 +1,63 @@
+/* eslint-disable no-multiple-empty-lines */
+
+// INSTRUCTIONS - PLEASE READ!!
+// Here are some challenges. Solve them from top to bottom
+
+// **Each time** you complete a challenge, please commit and push!
+// This is a good practise. Each time you make some progress in software
+// development, you want to save a snapshot.
+
+
+module.exports = function runChallenges(check) {
+  // Ex 1. Read the content of the email input
+  const email = null; // TODO: replace null, keep the email variable.
+
+
+
+  // Ex 2. Fill the content of the email input with your email
+
+
+
+
+  // Ex 3. Replace the email hint (next to the input) with 'This is my email now'
+  //       The text should be emphasized using a <strong> tag
+
+
+
+  // Ex 4. Add the .blue CSS class to the table header cells (th elements)
+
+
+
+  // Ex 5. Count the number of table body rows there are (team count!)
+  const teamCount = 0; // TODO: replace 0, keep the teamCount variable.
+
+
+
+  // Ex 6. Say there is a 15th team added to the table.
+  //       Add a row at the bottom, this new team should have zero points.
+
+
+
+  // Ex 7. Write some code to sum all points given to all teams
+  const sum = 0; // TODO: replace 0 with your sum result, keep the sum variable.
+
+
+
+  // Ex 8. Change the background color of all table header cells to #DDF4FF
+
+
+
+  // Ex 9. Remove the "Email:" label from the DOM
+
+
+
+
+
+
+
+
+
+
+
+  check(email, teamCount, sum); // Checking exercise answers. DO NOT MODIFY THIS LINE
+};
