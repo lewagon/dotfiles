4b153de49446709b2a67dc267840136bf827426a
Alex Benoit <alex@lewagon.com>
2017-10-17 12:40:51 +0100

    updated tests for my map

--- 01-Ruby/03-Iterators-Blocks/02-About-blocks/spec/block_gym_spec.rb
+++ 01-Ruby/03-Iterators-Blocks/02-About-blocks/spec/block_gym_spec.rb
@@ -20,20 +20,20 @@ describe "#timer_for" do
 end
 
 describe "#my_map" do
-  it "should upcase all elements of an array when passed `upcase` in the block" do
-    array = %w(a b c)
+  it "should upcase all elements of an array when passed `element.upcase` in the block" do
+    array = %w(alex John bOB)
     new_array = my_map(array) do |element|
       element.upcase
     end
-    expect(new_array).to eq(%w(A B C))
+    expect(new_array).to eq(%w(ALEX JOHN BOB))
   end
 
-  it "should downcase all elements of an array when passed `downcase` in the block" do
-    array = %w(A B C)
+  it "should multiply all integers with themselves when passed `element * element` in the block" do
+    array = [2, 4, 6, 8]
     new_array = my_map(array) do |element|
-      element.downcase
+      element * element
     end
-    expect(new_array).to eq(%w(a b c))
+    expect(new_array).to eq([4, 16, 36, 64])
   end
 end
 
