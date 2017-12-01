8f2b5a9aea8d9e1aa2c59b8cb8c852cdfc4dd54b
Sébastien Saunier <seb@saunier.me>
2017-12-01 09:40:31 +0100

    bundle open will append the gem to the current Sublime Text active window

--- install.sh
+++ install.sh
@@ -40,11 +40,11 @@ cd "$CURRENT_DIR"
 setopt nocasematch
 if [[ ! `uname` =~ "darwin" ]]; then
   git config --global core.editor "subl -n -w $@ >/dev/null 2>&1"
-  echo 'export BUNDLER_EDITOR="subl $@ >/dev/null 2>&1"' >> zshrc
+  echo 'export BUNDLER_EDITOR="subl $@ >/dev/null 2>&1 -a"' >> zshrc
 else
   git config --global core.editor "'/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl' -n -w"
   bundler_editor="'/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl'"
-  echo "export BUNDLER_EDITOR=\"${bundler_editor}\"" >> zshrc
+  echo "export BUNDLER_EDITOR=\"${bundler_editor} -a\"" >> zshrc
 fi
 
 # Sublime Text
