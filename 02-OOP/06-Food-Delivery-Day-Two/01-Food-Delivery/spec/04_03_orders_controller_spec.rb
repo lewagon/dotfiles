ab7e117f86d98d45a10b569a7a95c5898e29f120
kevcha <kevin.chavanne@gmail.com>
2018-01-23 13:05:37 +0100

    Fix rspec idling with ruby > 2.3

--- 02-OOP/06-Food-Delivery-Day-Two/01-Food-Delivery/spec/04_03_orders_controller_spec.rb
+++ 02-OOP/06-Food-Delivery-Day-Two/01-Food-Delivery/spec/04_03_orders_controller_spec.rb
@@ -72,10 +72,10 @@ describe "OrdersController", :_order do
   end
 
   describe "#list_undelivered_orders" do
-    module Kernel; def gets; STDIN.gets; end; end
-
     it "should list undelivered orders (with meal, employee assigned and customer info)" do
       controller = OrdersController.new(meal_repository, employee_repository, customer_repository, order_repository)
+      module Kernel; def gets; STDIN.gets; end; end
+
       orders.drop(2).each do |order|
         expect(STDOUT).to receive(:puts).with(/#{customer_repository.find(order[4]).name}/)
       end
@@ -84,12 +84,13 @@ describe "OrdersController", :_order do
   end
 
   describe "#add" do
-    module Kernel; def gets; STDIN.gets; end; end
+
 
     it "should ask the user for a meal id, a customer id and an employee id to be assigned" do
       controller = OrdersController.new(meal_repository, employee_repository, customer_repository, order_repository)
-
+      module Kernel; def gets; STDIN.gets; end; end
       allow(STDIN).to receive(:gets).and_return("2", "2", "2")
+
       controller.add
 
       expect(order_repository.undelivered_orders.length).to eq(4)
