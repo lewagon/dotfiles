772b961628cd3f0d1418b4cee073cbdef0737c81
dbo <dimitribosch@hotmail.com>
2018-01-29 15:53:34 +0100

    s/array/ActiveRecord::Relation
    To avoid confusion, as students don't get why a `sort_by` method for instance doesn't work.

--- 03-AR-Database/03-ActiveRecord-Basics/Optional-01-Mapping-Existing-Database/app/queries.rb
+++ 03-AR-Database/03-ActiveRecord-Basics/Optional-01-Mapping-Existing-Database/app/queries.rb
@@ -1,13 +1,13 @@
 def reversed_sorted_artists
-  # TODO: return an array of Artist, in reversed alphabetical order
+  # TODO: return an ActiveRecord::Relation (array) of Artist, in reversed alphabetical order
 end
 
 def love_tracks
-  # TODO: return an array of Track which sings about Love
+  # TODO: return an ActiveRecord::Relation (array) of Track which sings about Love
 end
 
 def long_tracks(min_length)
-  # TODO: return an array of Track of at least `min_length` minutes
+  # TODO: return an ActiveRecord::Relation (array) of Track of at least `min_length` minutes
 end
 
 def top_five_artists(genre_name)
