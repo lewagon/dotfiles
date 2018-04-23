4c88bdb6b9d423c9a1e35f88754db8271f99b51f
Sébastien Saunier <seb@saunier.me>
2018-04-23 16:52:45 +0200

    Fixes #684

--- 01-Ruby/01-Programming-basics/04-Stupid-coaching/README.md
+++ 01-Ruby/01-Programming-basics/04-Stupid-coaching/README.md
@@ -7,7 +7,7 @@ Unfortunately though, the coach is a little stupid and can only exhibit the foll
 
 1. If you don't **ask** him something, but just **tell** him something (e.g. `"I met a girl last night"`), he will just answer back `"I don't care, get dressed and go to work!"`
 2. If you ask him something (e.g. `"Can I eat some pizza?"`), he won't be of much help either and he will tell you `"Silly question, get dressed and go to work!"`
-3. The **only** way to get rid of him is to tell him what he wants to hear: `"I am going to work right now!"`
+3. The **only** way to get rid of him is to tell him what he wants to hear: `"I am going to work right now!"`.
 
 Let's make a comparison between the **real world** and the **code world** on this exercise.
 
--- 01-Ruby/01-Programming-basics/04-Stupid-coaching/spec/coach_answer_spec.rb
+++ 01-Ruby/01-Programming-basics/04-Stupid-coaching/spec/coach_answer_spec.rb
@@ -1,37 +1,37 @@
 require "coach_answer"
 
 describe "The method coach_answer" do
-  it 'should return a String' do
+  it 'returns a String' do
     expect( coach_answer("Hello Coach!") ).to be_a String
   end
 
-  it 'should reply saying he does not care when you enter a statement' do
+  it 'replies saying he does not care when you enter a statement' do
     expect( coach_answer("I would love to eat some pizza!") ).to eq "I don't care, get dressed and go to work!"
   end
 
-  it 'should reply saying it\'s a silly question when you enter a question' do
+  it 'replies saying it\'s a silly question when you enter a question' do
     expect( coach_answer("Can I eat some pizza?") ).to eq "Silly question, get dressed and go to work!"
   end
 
-  it 'does not answer back when you tell him you are going to work' do
+  it 'does not answer back (i.e. answers with an empty string) when you tell him you are going to work' do
     expect( coach_answer( "I am going to work right now!" ) ).to eq ""
   end
 end
 
 describe "The method coach_answer_enhanced" do
-  it 'should return a String' do
+  it 'returns a String' do
     expect( coach_answer_enhanced("Hello Coach!") ).to be_a String
   end
 
-  it 'should reply saying he does not care when you enter a statement (and does not shout)' do
+  it 'replies saying he does not care when you enter a statement (and does not shout)' do
     expect( coach_answer("I would love to eat some pizza!") ).to eq "I don't care, get dressed and go to work!"
   end
 
-  it 'should reply saying it\'s a silly question when you enter a question (and does not shout)' do
+  it 'replies saying it\'s a silly question when you enter a question (and does not shout)' do
     expect( coach_answer("Can I eat some pizza?") ).to eq "Silly question, get dressed and go to work!"
   end
 
-  it 'does not answer back when you tell him you are going to work (and does not shout)' do
+  it 'does not answer back (i.e. answers with an empty string) when you tell him you are going to work (and does not shout)' do
     expect( coach_answer( "I am going to work right now!" ) ).to eq ""
   end
 
@@ -43,7 +43,7 @@ describe "The method coach_answer_enhanced" do
     expect( coach_answer_enhanced("CAN I EAT SOME PIZZA?") ).to eq "I can feel your motivation! Silly question, get dressed and go to work!"
   end
 
-  it 'does not answer back when you shout at him saying you are going to work' do
+  it 'does not answer back (i.e. answers with an empty string) when you shout at him saying you are going to work' do
     expect( coach_answer_enhanced( "I AM GOING TO WORK RIGHT NOW!" ) ).to eq ""
   end
 end
