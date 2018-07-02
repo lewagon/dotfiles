183c698ff638506c4d65893ab31b7da1829ed86f
Phelim Dunleavy <dunleavy.phelim@gmail.com>
2018-07-02 15:17:16 +0200

    add warmup quiz for rails day three

---
+++ 05-Rails/03-Rails-restaurant-reviews/01-CRUD-quiz/lib/quiz.rb
@@ -0,0 +1,39 @@
+# TODO: Below are five questions for you to answer. Read each of the questions
+# and make sure each of the methods return the correct answer.
+
+def crud
+  # TODO: The `Array` below has the four letters that make up the `CRUD`
+  # acronym. Update each element of the array so that you write out the
+  # four verbs that describe the CRUD actions.
+
+  ['C', 'R', 'U', 'D']
+end
+
+def generate_model_command
+  # TODO: Return a `String` with the command you would run in the terminal to
+  # generate a `Restaurant` model that has two fields: name(`String`) and
+  # stars(`Integer`).
+end
+
+def files_generated_by_model_generator
+  # TODO: Update the `Array` returned with the paths to the two files created
+  # for you when run the model generator for a `Restaurant` model (from the
+  # question above). Use `YYYYMMDDHHMMSS` for any timestamps.
+  [
+    '',
+    ''
+  ]
+end
+
+def crud_routing
+  # TODO: there are seven `CRUD` routes that we need to know by heart. But
+  # we don't want to write all of the in our routes. Return a `String` with
+  # the single line we would add in `config/routes.rb` to add all seven CRUD
+  # routes for our `Restaurant` model.
+end
+
+def controller_methods
+  # TODO: If we have all seven CRUD routes, we will also need seven instance
+  # methods in our `RestaurantsController`. Return an `Array` with the seven
+  # controller methods that go along with the CRUD routes.
+end
---
+++ 05-Rails/03-Rails-restaurant-reviews/01-CRUD-quiz/spec/quiz_spec.rb
@@ -0,0 +1,55 @@
+require "quiz"
+
+describe "quiz" do
+  describe "#crud" do
+    it "should return the correct answer" do
+      crud_verbs = [
+                     'create',
+                     'read',
+                     'update',
+                     'delete',
+                    ]
+      expect(crud.map(&:downcase)).to eq(crud_verbs)
+    end
+  end
+
+  describe "#generate_model_command" do
+    it "should return the correct answer" do
+      correct_commands = [
+                          'rails generate model Restaurant name:string stars:integer',
+                          'rails generate model Restaurant name stars:integer',
+                          'rails generate model restaurant name:string stars:integer',
+                          'rails generate model restaurant name stars:integer',
+                          'rails g model Restaurant name:string stars:integer',
+                          'rails g model Restaurant name stars:integer',
+                          'rails g model restaurant name:string stars:integer',
+                          'rails g model restaurant name stars:integer',
+                          ]
+      expect(correct_commands).to include(generate_model_command)
+    end
+  end
+
+  describe "#files_generated_by_model_generator" do
+    it "should return the correct answer" do
+      files_generated = [
+                          'app/models/restaurant.rb',
+                          'db/migrate/YYYYMMDDHHMMSS_create_restaurants.rb'
+                        ]
+
+      expect(files_generated_by_model_generator.sort).to eq(files_generated)
+    end
+  end
+
+  describe "#crud_routing" do
+    it "should return the correct answer" do
+      expect(crud_routing).to eq('resources :restaurants')
+    end
+  end
+
+  describe "#controller_methods" do
+    it "should return the correct answer" do
+      instance_methods = %w(create destroy edit index new show update)
+      expect(controller_methods.sort).to eq(instance_methods)
+    end
+  end
+end
