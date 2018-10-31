56f4d720515af2b299bac2ad4d21299fd772ee07
Edward Schults <edward.schults@gmail.com>
2018-10-31 19:29:38 +0100

    remove solution

--- 04-Front-End/07-JavaScript-Plugins/02-Weather/lib/app.js
+++ 04-Front-End/07-JavaScript-Plugins/02-Weather/lib/app.js
@@ -1,16 +1,1 @@
-const API_KEY = '7cae54b659bd5361b8e7bb9973d5a412';
-
-const icon = document.getElementById('icon');
-const temperature = document.getElementById('temperature');
-const description = document.getElementById('description');
-const city = document.getElementById('city');
-
-fetch(`http://api.openweathermap.org/data/2.5/weather?q=Paris&appid=${API_KEY}`)
-  .then(response => response.json())
-  .then((data) => {
-    console.log(data);
-    icon.src = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
-    temperature.innerText = `${Math.round(data.main.temp) - 273}°C`;
-    description.innerText = data.weather[0].description;
-    city.innerText = data.name;
-  });
+// TODO: Put your JS code in here
