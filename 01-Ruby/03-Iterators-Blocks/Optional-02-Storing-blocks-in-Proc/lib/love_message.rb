82e5cfa268fbb6c3d8086e61eda994c64fd4e7ec
Clara Morgeneyer <clara_morgeneyer@web.de>
2019-03-06 15:12:11 +0100

    Add two examples on how to call the methods in proc exercise

--- 01-Ruby/03-Iterators-Blocks/Optional-02-Storing-blocks-in-Proc/lib/love_message.rb
+++ 01-Ruby/03-Iterators-Blocks/Optional-02-Storing-blocks-in-Proc/lib/love_message.rb
@@ -7,6 +7,9 @@ def tell_mum(&message_blk)
   # => Should call #tell of course
 end
 
+# Example
+# tell_mum {"I love you"}
+
 def tell_with_proc(who, message_blk)
   # TODO: return message addressed to who
   # => Message is passed as Proc, not as block
@@ -16,3 +19,7 @@ def tell_mum_with_proc(message_blk)
   # TODO: return message addressed to your mum
   # => Should call #tell_with_proc of course
 end
+
+# Example
+# love_block = Proc.new {"I love you"}
+# tell_mum_with_proc(love_block)
