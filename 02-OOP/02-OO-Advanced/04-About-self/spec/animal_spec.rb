a5f1641c441ee2e0dbd5c74ed0d0d510c16221de
Lucien George <luciengeorge95@gmail.com>
2019-07-17 15:15:55 +0100

    ascii art about self challenge

--- 02-OOP/02-OO-Advanced/04-About-self/spec/animal_spec.rb
+++ 02-OOP/02-OO-Advanced/04-About-self/spec/animal_spec.rb
@@ -1,5 +1,20 @@
 require "animal"
 
+BONUS = <<-eos
+
+     #####
+    #### _\\_  ________
+    ##=-[.].]| \\      \\
+    #(    _\\ |  |------|
+     #   __| |  ||||||||
+      \\  _/  |  ||||||||
+   .--'--'-. |  | ____ |
+  / __      `|__|[o__o]|
+_(_____________ /____\\____
+
+
+eos
+
 describe Animal do
   let(:fox) { Animal.new.name("Fox").color("red").natural_habitat("forest").species("mammal") }
 
