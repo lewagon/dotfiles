bc7549698035e4ed989298d0fbad942344d8a5c7
Sébastien Saunier <seb@saunier.me>
2019-08-18 18:57:33 +0200

    Remove blank space (not needed)

--- 01-Ruby/01-Programming-basics/03-Age-in-days/lib/age_in_days.rb
+++ 01-Ruby/01-Programming-basics/03-Age-in-days/lib/age_in_days.rb
@@ -2,7 +2,6 @@
 # Ruby library, giving you access to the Date class.
 require 'date'
 
-
 def age_in_days(day, month, year)
   # TODO: return the age expressed in days given the day, month, and year of birth
 end
