8984d78e050e86b885726756c09f758b8fa68693
Sébastien Saunier <seb@saunier.me>
2019-08-18 12:16:05 +0200

    Fix duplicate path in ActiveRecord::Migrator.migrations_paths

--- 03-AR-Database/utils.rb
+++ 03-AR-Database/utils.rb
@@ -1,6 +1,6 @@
 class FullStackChallengesUtils
-  def self.rake_migrate(db_namespace, file)
-    ActiveRecord::Migrator.migrations_paths << File.expand_path(file, 'db/migrate')
+  def self.rake_migrate(db_namespace, folder)
+    ActiveRecord::Migrator.migrations_paths = [File.join(folder, 'db/migrate')]
     ActiveRecord::Migration.verbose = true
     version = ENV['VERSION'] ? ENV['VERSION'].to_i : nil
     args = [ActiveRecord::Migrator.migrations_paths]
@@ -9,9 +9,9 @@ class FullStackChallengesUtils
     db_namespace["schema:dump"].invoke
   end
 
-  def self.spec_helper_migrate(version, file)
+  def self.spec_helper_migrate(version, folder)
     ActiveRecord::Migration.verbose = false
-    ActiveRecord::Migrator.migrations_paths << File.expand_path(file, '../db/migrate')
+    ActiveRecord::Migrator.migrations_paths = [File.join(folder, '../db/migrate')]
     args = [ActiveRecord::Migrator.migrations_paths]
     args << ActiveRecord::SchemaMigration if ActiveRecord.version.to_s >= "6.0.0"
     ActiveRecord::MigrationContext.new(*args).migrate(version)
