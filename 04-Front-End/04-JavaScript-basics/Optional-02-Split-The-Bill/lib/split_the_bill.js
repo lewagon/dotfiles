085491e7b4bfa8bfb100edb2a5acc5db7866503f
Bruno Parga <brunoparga@gmail.com>
2019-08-18 02:34:40 -0300

    Switch terminology from Ruby to JavaScript

--- 04-Front-End/04-JavaScript-basics/Optional-02-Split-The-Bill/lib/split_the_bill.js
+++ 04-Front-End/04-JavaScript-basics/Optional-02-Split-The-Bill/lib/split_the_bill.js
@@ -1,5 +1,5 @@
 const splitTheBill = (group) => {
-  // TODO: implement the method and return a hash
+  // TODO: implement the function and return an Object
 };
 
 module.exports = splitTheBill; // Do not remove this line.
