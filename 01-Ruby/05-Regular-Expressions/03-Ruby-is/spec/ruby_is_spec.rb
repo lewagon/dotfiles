b4a915d67b88e745fc1fde60d72d9ae84b36e802
Capucine Scheidel-Buchet <capucine.scheidelbuchet@gmail.com>
2019-12-20 16:23:09 +0100

    New Regex Challenge

--- 01-Ruby/05-Regular-Expressions/03-Ruby-is/spec/ruby_is_spec.rb
+++ 01-Ruby/05-Regular-Expressions/03-Ruby-is/spec/ruby_is_spec.rb
@@ -1,14 +1,18 @@
 require "ruby_is"
 
 describe "find_nth_word" do
-  it "should return a regular expression extracting the nth word of a sentence" do
+  it "should return the third word of a sentence" do
     expect(find_nth_word(3, "I love Regex")).to eq "Regex"
+  end
+
+  it "should return the fifth word of a sentence" do
+    expect(find_nth_word(5, "So much fun with Regex")).to eq "Regex"
   end
 end
 
 
 describe "#ruby_is" do
   it "should return the description of Ruby" do
-    expect(ruby_is).downcase.to eq "a dynamic open source programming language with a focus on simplicity and productivity it has an elegant syntax that is natural to read and easy to write"
+    expect(ruby_is).to eq "a dynamic open source programming language with a focus on simplicity and productivity it has an elegant syntax that is natural to read and easy to write"
   end
 end
