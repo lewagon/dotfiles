0e0dfe4889b408562304cc13065c682c644c6632
Edward Schults <edward.schults@gmail.com>
2020-01-03 11:56:24 +0100

    move TODOs in the body

--- 01-Ruby/05-Regular-Expressions/02-Time-complexity/lib/time_complexity.rb
+++ 01-Ruby/05-Regular-Expressions/02-Time-complexity/lib/time_complexity.rb
@@ -1,9 +1,9 @@
-# TODO: return index of book_to_find in the array of books
-# Use each_with_index
 def find_book(books, book_to_find)
+  # TODO: return index of `book_to_find` in the array of `books`
+  # Use `each_with_index`
 end
 
-# TODO: return index of book_to_find in the array of books
-# Use binary search routine
 def find_book_enhanced(books, book_to_find)
+  # TODO: return index of `book_to_find` in the array of `books`
+  # Use binary search
 end
