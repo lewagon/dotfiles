1232591daf9db871ed9a299bcc8c8d46a186d0c6
Capucine Scheidel-Buchet <capucine.scheidelbuchet@gmail.com>
2020-01-09 18:21:12 +0100

    02-Anagrams removed

--- 01-Ruby/05-Regular-Expressions/02-Anagrams/lib/anagrams.rb
+++
@@ -1,7 +0,0 @@
-def anagrams?(a_string, another_string)
-  # TODO: implement the obvious method to test if two words are anagrams
-end
-
-def anagrams_on_steroids?(a_string, another_string)
-  # TODO: implement the improved method
-end
