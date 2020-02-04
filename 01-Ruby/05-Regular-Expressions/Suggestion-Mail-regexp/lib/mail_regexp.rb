03d772f22f9a42317c841f3dd906482c2733d76e
Capucine Scheidel-Buchet <capucine.scheidelbuchet@gmail.com>
2020-02-04 16:47:02 +0100

    new challenge email regex

--- 01-Ruby/05-Regular-Expressions/Suggestion-Mail-regexp/lib/mail_regexp.rb
+++ 01-Ruby/05-Regular-Expressions/Suggestion-Mail-regexp/lib/mail_regexp.rb
@@ -1,71 +1,20 @@
 def valid?(email)
   # TODO: return true is the email is valid, false otherwise
-  email.match?(/^([^@]+)@([^@]+)\.(\w+)$/)
 end
 
 def clean_database(emails)
   # TODO: return an array with the valid emails only
-  emails.select { |email| valid?(email) }
 end
 
 def group_by_tld(emails)
   # TODO: return a Hash with emails grouped by TLD
-  emails.group_by { |email| email.match(/.(\w+)$/)[1] }
 end
 
 def compose_mail(email)
   # TODO: return a Hash with username, domain and tld extracted from email
-  match_data = email.match(/^(?<name>\w+)@(?<domain>\w+).(?<tld>\w+)$/)
-  return {
-    username: match_data[:name],
-    domain: match_data[:domain],
-    tld: match_data[:tld]
-  }
-end
-
-
-LOCALES = {
-  en: {
-    subject: "Our website is online",
-    body: "Come and visit us!",
-    cheers: "Cheers",
-    signature: "The Team"
-  },
-  fr: {
-    subject: "Notre site est en ligne",
-    body: "Venez nous rendre visite !",
-    cheers: "A bientot",
-    signature: "L'équipe"
-  },
-  de: {
-    subject: "Unsere Website ist jetzt online",
-    body: "Komm und besuche uns!",
-    cheers: "Prost",
-    signature: "Das Team"
-  }
-}
-
-def translate(word, language)
-  if LOCALES[language.to_sym].nil?
-    translations = LOCALES[:en]
-  else
-    translations = LOCALES[language.to_sym]
-  end
-  return translations[word]
 end
 
 def compose_mail_translated(email)
   # TODO: return a Hash with username, domain and tld extracted from email
   # TODO: translate subject, body, cheers and signature, according to TLD
-  match_data = email.match(/^(?<name>\w+)@(?<domain>\w+).(?<tld>\w+)$/)
-  language = match_data[:tld].to_sym
-  return {
-    username: match_data[:name],
-    domain: match_data[:domain],
-    tld: match_data[:tld],
-    subject: translate(:subject, language),
-    body: translate(:body, language),
-    cheers: translate(:cheers, language),
-    signature: translate(:signature, language)
-  }
 end
