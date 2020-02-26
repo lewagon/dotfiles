ea17be9d95fa897c1efcfb198fe26e82b1e33c7d
Hadrien Matringe <hadrien.matringe@gmail.com>
2020-02-26 10:17:47 +0100

    Make cookbook orders_controller spec #list_my_orders order insensitive

--- 02-OOP/05-Food-Delivery-Day-One/01-Food-Delivery/spec/04_03_orders_controller_spec.rb
+++ 02-OOP/05-Food-Delivery-Day-One/01-Food-Delivery/spec/04_03_orders_controller_spec.rb
@@ -104,7 +104,7 @@ describe "OrdersController", :_order do
     it "should list Ringo's undelivered orders" do
       controller = OrdersController.new(meal_repository, employee_repository, customer_repository, order_repository)
       ringo = employee_repository.find(3)  # ringo is a delivery guy
-      expect(STDOUT).to receive(:puts).with(/Paul McCartney.*Calzone/)
+      expect(STDOUT).to receive(:puts).with(/(Paul McCartney||Calzone).*(Calzone||Paul McCartney)/)
       controller.list_my_orders(ringo)
     end
   end
