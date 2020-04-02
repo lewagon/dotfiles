86256ddcf848b44205146ed443dc97273079b4e8
Damien Milon <milon.damien@gmail.com>
2020-04-02 12:27:09 +0200

    Solved Active Record Basics Model exercise

--- 03-AR-Database/03-ActiveRecord-Basics/03-Models/app/controllers/posts_controller.rb
+++ 03-AR-Database/03-ActiveRecord-Basics/03-Models/app/controllers/posts_controller.rb
@@ -6,28 +6,67 @@ class PostsController
   end
 
   def index
-    # DO NOT WRITE SQL QUERIES
-    # TODO: gather all posts from the database
-    # TODO: give them to the view to be printed
+    display_posts
   end
 
   def create
-    # DO NOT WRITE SQL QUERIES
-    # TODO: create a post
+    # Ask the user for a post title (view)
+    title = @view.ask_for(:title)
+    # Ask the user for a post url (view)
+    url = @view.ask_for(:url)
+    # Create the post (model)
+    # Save the post to the database (model)
+    post = Post.create(title: title, url: url, votes: 0)
+    display_posts
   end
 
   def update
-    # DO NOT WRITE SQL QUERIES
-    # TODO: update a post
+    id = ask_for_id
+    # Ask the user for a new title (view)
+    title = @view.ask_for(:title)
+    # Ask the user for a new url (view)
+    url = @view.ask_for(:url)
+    # Fetch the post from DB thanks to the id (model)
+    post = Post.find(id)
+    # Update this post title and url (model)
+    # Save the updated post to the DB (model)
+    post.update(title: title, url: url)
+    display_posts
   end
 
   def destroy
-    # DO NOT WRITE SQL QUERIES
-    # TODO: destroy a post
+    # Ask the user for and id (view)
+    id = ask_for_id
+    # Fetch the post from the DB thanks to the id (model)
+    # Destroy this post (model)
+    Post.destroy(id)
+    display_posts
   end
 
   def upvote
-    # DO NOT WRITE SQL QUERIES
-    # TODO: increment the `votes` column for a post
+    # Ask the user for an id (view)
+    id = ask_for_id
+    # Fetch the post from the DB thanks to the id (model)
+    post = Post.find(id)
+    # Upvote the post => increment the votes integer by one (model)
+    post.update(votes: post.votes + 1)
+    # Save the post to the DB
+    post.save
+    display_posts
+  end
+
+  private
+
+  def display_posts
+    # Fetch all posts from DB (model)
+    posts = Post.all
+    # Display these posts (view)
+    @view.display(posts)
+  end
+
+  def ask_for_id
+    display_posts
+    # Ask the user for an id (view)
+    @view.ask_for(:id).to_i
   end
 end
--- 03-AR-Database/03-ActiveRecord-Basics/03-Models/app/views/posts_view.rb
+++ 03-AR-Database/03-ActiveRecord-Basics/03-Models/app/views/posts_view.rb
@@ -1,3 +1,13 @@
 class PostsView
-  # TODO: implement some methods here when controller needs to `puts` or `gets`
+  def ask_for(stuff)
+    puts "#{stuff}?"
+    print "> "
+    gets.chomp
+  end
+
+  def display(posts)
+    posts.each do |post|
+      puts "#{post.id} - #{post.title} - #{post.votes} votes - #{post.url}"
+    end
+  end
 end
