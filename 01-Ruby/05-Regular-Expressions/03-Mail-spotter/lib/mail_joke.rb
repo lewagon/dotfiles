be4963830a5d15af4843f96a3ea1200936986c3b
Capucine Scheidel-Buchet <capucine.scheidelbuchet@gmail.com>
2020-05-25 15:58:19 +0200

    New Regex Email: added legacy challenges

---
+++ 01-Ruby/05-Regular-Expressions/03-Mail-spotter/lib/mail_joke.rb
@@ -0,0 +1,3 @@
+def mail_joke(email)
+  # TODO: Return (not print!) a joke suited to the email provided
+end
---
+++ 01-Ruby/05-Regular-Expressions/Optional-01-Provider-grouping/lib/provider_grouping.rb
@@ -0,0 +1,7 @@
+def group_mails(emails)
+  # TODO: group email by provider
+end
+
+def provider?(email, provider)
+  # TODO: return true if email is of given provider
+end
