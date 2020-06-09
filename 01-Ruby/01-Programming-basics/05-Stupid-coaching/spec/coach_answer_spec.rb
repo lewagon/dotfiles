caa418a23378ddde9db78cb3891be814645bbc83
Edward Schults <edward.schults@gmail.com>
2020-06-09 08:50:28 +0200

    fixes lewagon/teachers#335

--- 01-Ruby/01-Programming-basics/05-Stupid-coaching/spec/coach_answer_spec.rb
+++ 01-Ruby/01-Programming-basics/05-Stupid-coaching/spec/coach_answer_spec.rb
@@ -24,15 +24,15 @@ describe "The method coach_answer_enhanced" do
   end
 
   it 'replies saying he does not care when you enter a statement (and does not shout)' do
-    expect( coach_answer("I would love to eat some pizza!") ).to eq "I don't care, get dressed and go to work!"
+    expect( coach_answer_enhanced("I would love to eat some pizza!") ).to eq "I don't care, get dressed and go to work!"
   end
 
   it 'replies saying it\'s a silly question when you enter a question (and does not shout)' do
-    expect( coach_answer("Can I eat some pizza?") ).to eq "Silly question, get dressed and go to work!"
+    expect( coach_answer_enhanced("Can I eat some pizza?") ).to eq "Silly question, get dressed and go to work!"
   end
 
   it 'does not answer back (i.e. answers with an empty string) when you tell him you are going to work (and does not shout)' do
-    expect( coach_answer( "I am going to work right now!" ) ).to eq ""
+    expect( coach_answer_enhanced( "I am going to work right now!" ) ).to eq ""
   end
 
   it 'prepends a motivation statement before saying he does not care when you shout a statement at him' do
