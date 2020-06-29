955368a472a425937877427205eae68e5dd33134
bryan leighton <bryan.leigh77@gmail.com>
2020-06-29 14:21:26 -0700

    k8s script

--- scripts/aks_cluster_keyvault.sh
+++ scripts/aks_cluster_keyvault.sh
@@ -1,13 +1,13 @@
 
 
 if [ "$#" -eq 0 ]; then
-    echo "USAGE: ./aks_cluster_keyvault.sh [\$RG_NAME] \$CLUSTER_NAME \$K8S_VERSION"
-    exit 0
+  echo "USAGE: ./aks_cluster_keyvault.sh [\$RG_NAME] [\$CLUSTER_NAME] [\$K8S_VERSION]"
+  exit 0
 fi
 
 export RG_NAME=$1
 export CLUSTER_NAME=$2
-export K8S_V=${3:-'1.17.3'}
+export K8S_V=${3:-'1.18.2'}
 export LOC=westus
 export KEYVAULT_NAME=kv$RG_NAME-$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 10 | head -n 1)
 
@@ -16,21 +16,23 @@ az group create \
   --name $RG_NAME \
   --location $LOC
 
-
+echo "Creating AKS Cluster"
 # Create AKS Cluster
 az aks create \
   --name $CLUSTER_NAME \
   --location $LOC \
   --resource-group $RG_NAME \
-  --nodepool-name linux --node-count 1 \
+  --nodepool-name linux --node-count 3 \
   --enable-addons monitoring \
   --generate-ssh-keys \
   --kubernetes-version $K8S_V \
   --query "{ name: name, resourceGroup: resourceGroup }"
 
+echo "Setting K8s Context"
 # Switch Context to newly created cluster
 az aks get-credentials --name $CLUSTER_NAME -g $RG_NAME
 
+echo "Creating Key Vault"
 # create Key vault
 export KEYVAULT_ID=$(az keyvault create --name $KEYVAULT_NAME --resource-group $RG_NAME --location $LOC --query id -o tsv)
 # Set Secrets
@@ -38,7 +40,7 @@ az keyvault secret set --vault-name $KEYVAULT_NAME --name databasePassword --val
 az keyvault secret set --vault-name $KEYVAULT_NAME --name storageToken --value $(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 10 | head -n 1)
 
 # Create Service Principal
-export SP_CLIENT_ID=$(az ad sp create-for-rbac --name "$RG_NAME-sp" --role="Contributor" --scopes $KEYVAULT_ID --query "{appId: appId}" -o tsv)
+export SP_CLIENT_ID=$(az ad sp create-for-rbac --name "$RG_NAME-sp" --role="Reader" --scopes $KEYVAULT_ID --query "{appId: appId}" -o tsv)
 export SP_CLIENT_SECRET=$(az ad sp credential reset --name $RG_NAME-sp --credential-description $RG_NAME --query password -o tsv)
 
 # Add credentials to K8s cluster
@@ -52,5 +54,8 @@ az keyvault set-policy -n $KEYVAULT_NAME --certificate-permissions get --spn $SP
 az keyvault set-policy -n $KEYVAULT_NAME --key-permissions get --spn $SP_CLIENT_ID
 
 
-
+#
+echo "SP_CLIENT_ID: \n ${SP_CLIENT_ID}"
+echo "SP_CLIENT_SECRET: \n ${SP_CLIENT_SECRET}"
+echo "KEYVAULT_NAME: \n ${KEYVAULT_NAME}"
 
