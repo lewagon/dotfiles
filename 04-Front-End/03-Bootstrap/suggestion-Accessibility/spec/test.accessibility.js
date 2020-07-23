b4641a76473d6f8e6beda389ed61512174c240bf
Capucine Scheidel-Buchet <capucine.scheidelbuchet@gmail.com>
2020-07-23 15:55:52 +0200

    Accessibility Challenge: rake removed

--- 04-Front-End/03-Bootstrap/suggestion-Accessibility/spec/test.accessibility.js
+++
@@ -1,7 +0,0 @@
-const assert = require("assert");
-
-describe("Semantics", () => {
-  it("should return 'even' for 0", () => {
-    assert.equal(document.querySelector('h1').textContent, 'The POUR Principles');
-  });
-});
