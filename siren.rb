ac6ba714b89783fca85c78a29279451dd42ffd16
Alexandre Bouvier <alexandre.b2506@gmail.com>
2020-11-10 10:50:18 +0100

    Adds shell for siren and siret

---
+++ siren.rb
@@ -0,0 +1,9 @@
+require 'clipboard'
+
+siren = "792677809"
+
+Clipboard.copy(siren)
+
+puts "792 677 809"
+puts ""
+puts "Siren was copy to your clipboard"
---
+++ siret.rb
@@ -0,0 +1,9 @@
+require 'clipboard'
+
+siret = "79267780900016"
+
+Clipboard.copy(siret)
+
+puts "79267780900016"
+puts ""
+puts "Siren was copy to your clipboard"
