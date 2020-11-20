10b379beb489f0c35d9c497b6f87ef8de6eee312
Alexandre Bouvier <alexandre.b2506@gmail.com>
2020-11-20 11:00:27 +0100

    Updates aliases

--- siren.rb
+++
@@ -1,9 +0,0 @@
-require 'clipboard'
-
-siren = "792677809"
-
-Clipboard.copy(siren)
-
-puts "792 677 809"
-puts ""
-puts "Siren was copy to your clipboard"
--- siret.rb
+++
@@ -1,9 +0,0 @@
-require 'clipboard'
-
-siret = "79267780900016"
-
-Clipboard.copy(siret)
-
-puts "79267780900016"
-puts ""
-puts "Siren was copy to your clipboard"
