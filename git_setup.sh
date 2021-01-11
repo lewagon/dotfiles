1fba34281d003fba2ee4303b9826578887eecce8
ElvisDot <elvis.doettinger@gmail.com>
2021-01-11 13:41:53 +0100

    Add basic ci for json

--- git_setup.sh
+++ git_setup.sh
@@ -1,11 +1,13 @@
+#!/bin/sh
+
 echo "Type in your first and last name (no accent or special characters - e.g. 'ç'): "
-read full_name
+read -r full_name
 
 echo "Type in your email address (the one used for your GitHub account): "
-read email
+read -r email
 
-git config --global user.email $email
-git config --global user.name $full_name
+git config --global user.email "$email"
+git config --global user.name "$full_name"
 
 git add .
 git commit --message "My identity for @lewagon in the gitconfig"
