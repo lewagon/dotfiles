a89b7551b33302a65ffd37cc328ee287d6b7df0c
Damien Milon <37899667+dmilon@users.noreply.github.com>
2021-04-05 17:35:16 +0200

    remove .git extension

--- install.sh
+++ install.sh
@@ -34,7 +34,7 @@ mkdir -p "$ZSH_PLUGINS_DIR" && cd "$ZSH_PLUGINS_DIR"
 if [ ! -d "$ZSH_PLUGINS_DIR/zsh-syntax-highlighting" ]; then
   echo "-----> Installing zsh plugin 'zsh-syntax-highlighting'..."
   git clone https://github.com/zsh-users/zsh-autosuggestions
-  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting
 fi
 cd "$CURRENT_DIR"
 
