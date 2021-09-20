89dbaf4b9848733588fcb23e82163f1311c0eda8
Xavier Rosee <xrosee&live.cn>
2021-09-20 14:14:53 +0200

    :fire: remove useless files and function calls

--- install.sh
+++ install.sh
@@ -3,26 +3,35 @@
 # make sure directory path is ok when script is started from anywhere
 MYDIR="$(dirname -- "$0")"
 
+#git config
+rm -rf ~/.gitconfig
+cp $MYDIR/gitconfig ~/.gitconfig
+
 # .zshrc install
 rm -rf ~/.zshrc
 cp $MYDIR/zshrc ~/.zshrc
 
 # .vimrc install
+rm -rf ~/.vimrc
 cp $MYDIR/vimrc ~/.vimrc
 
-# .vim install
-rm -rf ~/.vim || true
-cp -r $MYDIR/vim ~/.vim
+# .tm_properties install
+rm -rf ~/.tm_properties
+cp $MYDIR/tm_properties ~/.tm_properties
+
+# .rspec install
+rm -rf ~/.rspec
+cp $MYDIR/rspec ~/.rspec
+
+# .irbrc install
+rm -rf ~/.irbrc
+cp $MYDIR/irbrc ~/.irbrc
 
 # .profile and .zprofile install
 rm -rf ~/.profile || true
 cp -r $MYDIR/profile ~/.profile
 rm -rf ~/.zprofile || true
 cp -r $MYDIR/zprofile ~/.zprofile
-
-# nvim install
-rm -rf ~/.config/nvim || true
-cp -r $MYDIR/nvim ~/.config/nvim
 
 # iterm2 config (sensible for mac only)
 case $OSTYPE in
@@ -40,7 +49,5 @@ if [ -f ~/.zshrc ]; then
     echo "[ -f ~/.bash_aliases ] && source ~/.bash_aliases" >> ~/.zshrc
 fi
 
-# Install coc extensions
-vim -c 'CocInstall -sync coc-json coc-html coc-css coc-tsserver coc-prettier coc-go coc-styled-components coc-graphql|q'
 
 echo "dotfiles finished installing!"
