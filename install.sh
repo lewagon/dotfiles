4bf70c921a47d43d67967ad22cb59b911dbc58a6
Xavier Rosee <xrosee@live.cn>
2022-01-26 23:51:31 +0100

    use symbolic links instead of moving files

--- install.sh
+++ install.sh
@@ -5,63 +5,63 @@ MYDIR="$(dirname -- "$0")"
 
 #git config
 rm -rf ~/.gitconfig || true
-ln -s ~/.gitconfig ~/workspace/xavierosee/dotfiles/gitconfig
+ln -s ~/workspace/xavierosee/dotfiles/gitconfig ~/.gitconfig
 
 # .zshrc install
 rm -rf ~/.zshrc || true
-ln -s ~/.zshrc ~/workspace/xavierosee/dotfiles/zshrc
+ln -s ~/workspace/xavierosee/dotfiles/zshrc ~/.zshrc
 
 # .vimrc install
 rm -rf ~/.vimrc || true
-ln -s ~/.vimrc ~/workspace/xavierosee/dotfiles/init.vim
+ln -s ~/workspace/xavierosee/dotfiles/init.vim ~/.vimrc
 
 # .tm_properties install
 rm -rf ~/.tm_properties || true
-ln -s ~/.tm_properties ~/workspace/xavierosee/dotfiles/tm_properties
+ln -s ~/workspace/xavierosee/dotfiles/tm_properties ~/.tm_properties
 
 # .rspec install
 rm -rf ~/.rspec || true
-ln -s ~/.rspec ~/workspace/xavierosee/dotfiles/rspec
+ln -s ~/workspace/xavierosee/dotfiles/rspec ~/.rspec
 
 # .irbrc install
 rm -rf ~/.irbrc || true
-ln -s ~/.irbrc ~/workspace/xavierosee/dotfiles/irbrc
+ln -s ~/workspace/xavierosee/dotfiles/irbrc ~/.irbrc
 
 # .profile and .zprofile install
 rm -rf ~/.profile || true
-ln -s ~/.profile ~/workspace/xavierosee/dotfiles/profile
+ln -s ~/workspace/xavierosee/dotfiles/profile ~/.profile
 rm -rf ~/.zprofile || true
-ln -s ~/.zprofile ~/workspace/xavierosee/dotfiles/zprofile
+ln -s ~/workspace/xavierosee/dotfiles/zprofile ~/.zprofile
 
 # installing z script
 rm -rf ~/z.sh || true
-ln -s ~/z.sh ~/workspace/xavierosee/dotfiles/z
+ln -s ~/workspace/xavierosee/dotfiles/z ~/z.sh
 
 # iterm2 config (sensible for mac only)
 case $OSTYPE in
     darwin*)
         # Specify the preferences directory
-        defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/dotfiles/iterm2"
+        defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/workspace/xavierosee/dotfiles/iterm2"
         # Tell iTerm2 to
         defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
         ;;
 esac
 
 # .bash_aliases install
-rm -rf ∞/.aliases || true
-ln -s ~/.aliases ~/workspace/xavierosee/dotfiles/aliases
+rm -rf ~/.aliases || true
+ln -s ~/workspace/xavierosee/dotfiles/aliases ~/.aliases
 
 # .ackrc install
 rm -rf ~/.ackrc || true
-ln -s ~/.ackrc ~/workspace/xavierosee/dotfiles/ackrc
+ln -s ~/workspace/xavierosee/dotfiles/ackrc ~/.ackrc
 
 # starship.toml install
 rm -rf ~/.config/starship.toml || true
-ln -s ~/.config/starship.toml ~/workspace/xavierosee/dotfiles/starship.toml
+ln -s ~/workspace/xavierosee/dotfiles/starship.toml ~/.config/starship.toml
 
 # init.vim install
 mv ~/.config/nvim /tmp || true
 mkdir ~/.config/nvim
-ln -s ~/.config/nvim/init.vim ~/workspace/xavierosee/dotfiles/init.vim
+ln -s ~/workspace/xavierosee/dotfiles/init.vim ~/.config/init.vim
 
 echo " ##### dotfiles finished installing! ##### "
