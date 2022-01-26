66e41c4a0ff3ff5c093ab71c264cceee3f865ec9
Xavier Rosee <xrosee@live.cn>
2022-01-26 19:33:54 +0100

    update to latest config

--- install.sh
+++ install.sh
@@ -4,46 +4,38 @@
 MYDIR="$(dirname -- "$0")"
 
 #git config
-rm -rf ~/.gitconfig
-cp $MYDIR/gitconfig ~/.gitconfig
+rm -rf ~/.gitconfig || true
+ln -s ~/.gitconfig ~/workspace/xavierosee/dotfiles/gitconfig
 
 # .zshrc install
-rm -rf ~/.zshrc
-cp $MYDIR/zshrc ~/.zshrc
+rm -rf ~/.zshrc || true
+ln -s ~/.zshrc ~/workspace/xavierosee/dotfiles/zshrc
 
 # .vimrc install
-rm -rf ~/.vimrc
-cp $MYDIR/vimrc ~/.vimrc
+rm -rf ~/.vimrc || true
+ln -s ~/.vimrc ~/workspace/xavierosee/dotfiles/init.vim
 
 # .tm_properties install
-rm -rf ~/.tm_properties
-cp $MYDIR/tm_properties ~/.tm_properties
+rm -rf ~/.tm_properties || true
+ln -s ~/.tm_properties ~/workspace/xavierosee/dotfiles/tm_properties
 
 # .rspec install
-rm -rf ~/.rspec
-cp $MYDIR/rspec ~/.rspec
+rm -rf ~/.rspec || true
+ln -s ~/.rspec ~/workspace/xavierosee/dotfiles/rspec
 
 # .irbrc install
-rm -rf ~/.irbrc
-cp $MYDIR/irbrc ~/.irbrc
+rm -rf ~/.irbrc || true
+ln -s ~/.irbrc ~/workspace/xavierosee/dotfiles/irbrc
 
 # .profile and .zprofile install
 rm -rf ~/.profile || true
-cp -r $MYDIR/profile ~/.profile
+ln -s ~/.profile ~/workspace/xavierosee/dotfiles/profile
 rm -rf ~/.zprofile || true
-cp -r $MYDIR/zprofile ~/.zprofile
+ln -s ~/.zprofile ~/workspace/xavierosee/dotfiles/zprofile
 
 # installing z script
-rm -rf ~/.z/sh || true
-cp -r $MYDIR/z ~/.z
-
-# .taskrc install
-rm -rf ~/.taskrc
-cp $MYDIR/taskrc ~/.taskrc
-
-# .bugwarriorrc install
-rm -rf ~/.taskrc
-cp $MYDIR/taskrc ~/.taskrc
+rm -rf ~/z.sh || true
+ln -s ~/z.sh ~/workspace/xavierosee/dotfiles/z
 
 # iterm2 config (sensible for mac only)
 case $OSTYPE in
@@ -56,11 +48,20 @@ case $OSTYPE in
 esac
 
 # .bash_aliases install
-rm -rf ∞/.aliases
-cp $MYDIR/aliases ~/.aliases
-if [ -f ~/.zshrc ]; then
-    echo "[ -f ~/.aliases ] && source ~/.aliases" >> ~/.zshrc
-fi
+rm -rf ∞/.aliases || true
+ln -s ~/.aliases ~/workspace/xavierosee/dotfiles/aliases
 
+# .ackrc install
+rm -rf ~/.ackrc || true
+ln -s ~/.ackrc ~/workspace/xavierosee/dotfiles/ackrc
+
+# starship.toml install
+rm -rf ~/.config/starship.toml || true
+ln -s ~/.config/starship.toml ~/workspace/xavierosee/dotfiles/starship.toml
+
+# init.vim install
+mv ~/.config/nvim /tmp || true
+mkdir ~/.config/nvim
+ln -s ~/.config/nvim/init.vim ~/workspace/xavierosee/dotfiles/init.vim
 
 echo " ##### dotfiles finished installing! ##### "
