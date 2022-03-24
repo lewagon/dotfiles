72e86eae726f5e096c1286284797d5b5cfbabe07
dylan wang <zywangdylan@gmail.com>
2022-03-24 14:43:32 +0800

    04-vue-geocoder template

---
+++ 04-Front-End/07-JavaScript-Plugins/04-Vue-Geocoder/lib/index.js
@@ -0,0 +1,21 @@
+// TODO: Import mapbox-gl
+
+// TODO: Setup Vue here
+
+// Disabled Vue production tip in the console
+Vue.config.productionTip = false
+
+const app = new Vue({
+    el: "#app",
+    data: {
+        // Your Data
+    },
+    methods: {
+        // [Optional] TODO: Create `insertMap` function to insert Map
+
+        // TODO: Create `geocode` function to fetch mapbox API
+
+        // TODO: Create `submit` function to be triggered when form submitted
+
+    }
+})
