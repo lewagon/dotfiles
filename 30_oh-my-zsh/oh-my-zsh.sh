1244258ae00d8cfc372c04f3bf8446f8d987197e
Miguel Hargreaves Pimenta <jmiguelpimenta@gmail.com>
2022-04-05 15:15:06 +0200

    edit oh my zsh plugins

--- 30_oh-my-zsh/oh-my-zsh.sh
+++ 30_oh-my-zsh/oh-my-zsh.sh
@@ -25,7 +25,7 @@ echo "Done"
 
 echo "Installing zsh-users plugins"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions
+git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 echo "Done"
 
