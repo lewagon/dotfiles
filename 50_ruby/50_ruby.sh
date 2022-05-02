ac231ddd9d19fdd4ec18aa1fcdc22a181f637957
Miguel Hargreaves Pimenta <jmiguelpimenta@icloud.com>
2022-05-02 15:51:19 -0300

    Update ruby gemfile

--- 50_ruby/50_ruby.sh
+++ 50_ruby/50_ruby.sh
@@ -6,7 +6,7 @@ echo "Installing Ruby 3.0.3"
 rbenv install 3.0.3
 echo "Done"
 
-echo "Installing Ruby 3.0.3"
+echo "Installing Ruby gems"
 bundle install --gemfile=50_ruby/Gemfile
 rm 50_ruby/Gemfile.lock
 echo "Done"
