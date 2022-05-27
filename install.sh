5c2ff6a562f53da52259510b200fc04430890cec
Xavier PICHETTI <45852401+zpichetti@users.noreply.github.com>
2022-05-27 22:26:58 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,65 +1,75 @@
+#!/bin/zsh
+
+# Define a function which rename a `target` file to `target.backup` if the file
+# exists and if it's a 'real' file, ie not a symlink
 backup() {
   target=$1
-  if [ -e "$target" ]; then           # Does the config file already exist?
-    if [ ! -L "$target" ]; then       # as a pure file?
-      mv "$target" "$target.backup"   # Then backup it
+  if [ -e "$target" ]; then
+    if [ ! -L "$target" ]; then
+      mv "$target" "$target.backup"
       echo "-----> Moved your old $target config file to $target.backup"
     fi
   fi
 }
 
-#!/bin/zsh
-for name in *; do
+symlink() {
+  file=$1
+  link=$2
+  if [ ! -e "$link" ]; then
+    echo "-----> Symlinking your new $link"
+    ln -s $file $link
+  fi
+}
+
+# For all files `$name` in the present folder except `*.sh`, `README.md`, `settings.json`,
+# and `config`, backup the target file located at `~/.$name` and symlink `$name` to `~/.$name`
+for name in aliases gitconfig irbrc rspec zprofile zshrc; do
   if [ ! -d "$name" ]; then
     target="$HOME/.$name"
-    if [[ ! "$name" =~ '\.sh$' ]] && [ "$name" != 'README.md' ] && [[ ! "$name" =~ '\.sublime-settings$' ]]; then
-      backup $target
-
-      if [ ! -e "$target" ]; then
-        echo "-----> Symlinking your new $target"
-        ln -s "$PWD/$name" "$target"
-      fi
-    fi
+    backup $target
+    symlink $PWD/$name $target
   fi
 done
 
-REGULAR="\\033[0;39m"
-YELLOW="\\033[1;33m"
-GREEN="\\033[1;32m"
-
-# zsh plugins
+# Install zsh-syntax-highlighting plugin
 CURRENT_DIR=`pwd`
 ZSH_PLUGINS_DIR="$HOME/.oh-my-zsh/custom/plugins"
 mkdir -p "$ZSH_PLUGINS_DIR" && cd "$ZSH_PLUGINS_DIR"
 if [ ! -d "$ZSH_PLUGINS_DIR/zsh-syntax-highlighting" ]; then
   echo "-----> Installing zsh plugin 'zsh-syntax-highlighting'..."
-  git clone git://github.com/zsh-users/zsh-syntax-highlighting.git
+  git clone https://github.com/zsh-users/zsh-autosuggestions
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting
 fi
 cd "$CURRENT_DIR"
 
-setopt nocasematch
-if [[ ! `uname` =~ "darwin" ]]; then
-  git config --global core.editor "subl -n -w $@ >/dev/null 2>&1"
-  echo 'export BUNDLER_EDITOR="subl $@ >/dev/null 2>&1 -a"' >> zshrc
+# Symlink VS Code settings and keybindings to the present `settings.json` and `keybindings.json` files
+# If it's a macOS
+if [[ `uname` =~ "Darwin" ]]; then
+  CODE_PATH=~/Library/Application\ Support/Code/User
+# Else, it's a Linux
 else
-  git config --global core.editor "'/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl' -n -w"
-  bundler_editor="'/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl'"
-  echo "export BUNDLER_EDITOR=\"${bundler_editor} -a\"" >> zshrc
+  CODE_PATH=~/.config/Code/User
+  # If this folder doesn't exist, it's a WSL
+  if [ ! -e $CODE_PATH ]; then
+    CODE_PATH=~/.vscode-server/data/Machine
+  fi
 fi
 
-# Sublime Text
-if [[ ! `uname` =~ "darwin" ]]; then
-  SUBL_PATH=~/.config/sublime-text-3
-else
-  SUBL_PATH=~/Library/Application\ Support/Sublime\ Text\ 3
+for name in settings.json keybindings.json; do
+  target="$CODE_PATH/$name"
+  backup $target
+  symlink $PWD/$name $target
+done
+
+# Symlink SSH config file to the present `config` file for macOS and add SSH passphrase to the keychain
+if [[ `uname` =~ "Darwin" ]]; then
+  target=~/.ssh/config
+  backup $target
+  symlink $PWD/config $target
+  ssh-add -K ~/.ssh/id_ed25519
 fi
-mkdir -p $SUBL_PATH/Packages/User $SUBL_PATH/Installed\ Packages
-backup "$SUBL_PATH/Packages/User/Preferences.sublime-settings"
-curl -k https://sublime.wbond.net/Package%20Control.sublime-package > $SUBL_PATH/Installed\ Packages/Package\ Control.sublime-package
-ln -s $PWD/Preferences.sublime-settings $SUBL_PATH/Packages/User/Preferences.sublime-settings
-ln -s $PWD/Package\ Control.sublime-settings $SUBL_PATH/Packages/User/Package\ Control.sublime-settings
-ln -s $PWD/SublimeLinter.sublime-settings $SUBL_PATH/Packages/User/SublimeLinter.sublime-settings
 
-zsh ~/.zshrc
+# Refresh the current terminal with the newly installed configuration
+exec zsh
 
-echo "👌  Carry on with git setup!"
+echo "👌 Carry on with git setup!"
