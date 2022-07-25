52601a1f6acac039278142ad22141abab2ad465a
Caio Andrade <andrade@hey.com>
2022-07-25 14:04:23 -0500

    Add absolute path pattern

--- 02-OOP/03-Cookbook-Day-One/02-Cookbook/spec/load_error_check.rb
+++ 02-OOP/03-Cookbook-Day-One/02-Cookbook/spec/load_error_check.rb
@@ -9,8 +9,9 @@ begin
 rescue LoadError => e
   describe "LoadError Check" do
     it 'requires files with no errors' do
-      backtrace_message = e.backtrace.first.gsub(/.*\/fullstack-challenges.*\d+-Cookbook/, '')
-      relative_path = e.path.gsub(/.*\/fullstack-challenges.*\d+-Cookbook/, '')
+      absolute_path_pattern = /.*\/fullstack-challenges.*\d+-Cookbook\//
+      backtrace_message = e.backtrace.first.gsub(absolute_path_pattern, '')
+      relative_path = e.path.gsub(absolute_path_pattern, '')
       raise RequireError.new("#{backtrace_message} failed to find #{relative_path}")
     end
   end
