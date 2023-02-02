b21287244cba7cc9cd1cc1e2e9910db287d06bcb
Miguel Hargreaves Pimenta <jmiguelpimenta@icloud.com>
2023-02-02 20:05:03 +0100

    refactor: add brew to path before mackup restore

--- 40_mackup/mackup.sh
+++ 40_mackup/mackup.sh
@@ -2,6 +2,9 @@
 
 sudo -v
 
+# Add Homebrew executable to PATH
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
 ln -s $HOME"/Library/Mobile Documents/com~apple~CloudDocs/Mackup/.mackup.cfg" ~
 
 mackup restore
