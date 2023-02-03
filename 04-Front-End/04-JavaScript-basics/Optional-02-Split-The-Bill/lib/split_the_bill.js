421dec3a1b5d6d15c467a3365c2e0cf05665c6f9
Thanh Tran <trandgthanh@gmail.com>
2023-02-03 19:14:45 +0100

    cleanup and empty folders (ready for setup day)

--- 04-Front-End/04-JavaScript-basics/Optional-02-Split-The-Bill/lib/split_the_bill.js
+++
@@ -1,5 +0,0 @@
-const splitTheBill = (group) => {
-  // TODO: implement the function and return an Object
-};
-
-module.exports = splitTheBill; // Do not remove this line.
--- 04-Front-End/06-HTTP-and-AJAX/04-Promo-Chat/lib/index.js
+++
@@ -1,4 +0,0 @@
-const batch = 1; // change to your own batch id
-const baseUrl = "https://wagon-chat.herokuapp.com/";
-
-// Your turn to code!
