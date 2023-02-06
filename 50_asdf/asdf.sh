34e2dba5ea560653d6469ef531b2a877385cfd58
Miguel Hargreaves Pimenta <jmiguelpimenta@icloud.com>
2023-02-06 16:13:08 +0100

    feat: add asdf script to install nodejs and ruby

---
+++ 50_asdf/asdf.sh
@@ -0,0 +1,10 @@
+#!/bin/zsh
+
+sudo -v
+
+# Add nodejs plugin
+asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
+# Add rubby plugin
+asdf plugin add ruby https://github.com/asdf-vm/asdf-ruby.git
+
+adsf install
