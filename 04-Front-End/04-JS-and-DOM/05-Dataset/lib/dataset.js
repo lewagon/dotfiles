e8a11e9ae4ac50e8b242138648bc57033a3d8d39
Thanh Tran <trandgthanh@gmail.com>
2023-02-07 17:03:00 +0100

    js & dom - remove dataset challenge

--- 04-Front-End/04-JS-and-DOM/05-Dataset/lib/dataset.js
+++
@@ -1,8 +0,0 @@
-const showNewPrice = () => {
-  // TODO: Add a price data attribute to the .card element in your HTML
-
-  // TODO: Select the data attribute to get the new price
-  // TODO: Replace the current price of 15€ with the new one!
-};
-
-showNewPrice(); // Do not remove!
