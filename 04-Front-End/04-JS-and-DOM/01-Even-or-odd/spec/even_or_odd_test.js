5ae9117d08cc060b55859d8f6d984034939c68cb
Thanh Tran <trandgthanh@gmail.com>
2023-02-28 18:17:58 +0100

    js-testing - revert to testing with node

---
+++ 04-Front-End/04-JS-and-DOM/01-Even-or-odd/spec/even_or_odd_test.js
@@ -0,0 +1,17 @@
+const assert = require("assert");
+const evenOrOdd = require("../lib/even_or_odd");
+
+describe("evenOrOdd", () => {
+  it("should return 'even' for 0", () => {
+    assert.equal(evenOrOdd(0), 'even');
+  });
+  it("should false 'odd' for 1", () => {
+    assert.equal(evenOrOdd(1), 'odd');
+  });
+  it("should return 'even' for 2", () => {
+    assert.equal(evenOrOdd(2), 'even');
+  });
+  it("should return 'odd' for 101", () => {
+    assert.equal(evenOrOdd(101), 'odd');
+  });
+});
