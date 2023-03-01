8d20ef54c39509c5b299eebfd691eaecc12c20e9
Thanh Tran <trandgthanh@gmail.com>
2023-03-01 15:23:24 +0100

    js & dom - review select-element & dom-manipulation

--- 04-Front-End/04-JS-and-DOM/02-Select-element/lib/select.js
+++ 04-Front-End/04-JS-and-DOM/02-Select-element/lib/select.js
@@ -6,7 +6,7 @@ import runChallenges from "../spec/select_examiner.js";
 
 // TODO 1. Open the `index.html` file and set an id to the France <li>
 
-export const franceElement = () => {
+const franceElement = () => {
   // TODO 2: select the France list item and return it!
 };
 
