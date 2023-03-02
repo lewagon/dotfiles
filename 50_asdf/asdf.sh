24a1556b51e3151a6a8efe8288b92d4f5de7fb5d
Miguel Hargreaves Pimenta <jmiguelpimenta@icloud.com>
2023-03-02 20:25:34 -0300

    feat: add yarn asdf plugin

--- 50_asdf/asdf.sh
+++ 50_asdf/asdf.sh
@@ -4,6 +4,8 @@ sudo -v
 
 # Add nodejs plugin
 asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
+# Add yarn plugin
+asdf plugin-add yarn
 # Add rubby plugin
 asdf plugin add ruby https://github.com/asdf-vm/asdf-ruby.git
 
