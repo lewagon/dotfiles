ad6d964c99d9c6025e0d75b4225a811b4f0a2cc1
Anne de Joly <anne.dejoly@edhec.com>
2023-03-02 17:12:05 +0000

    Remove solution

--- 04-Front-End/08-Templating/02-Recipe-book/lib/index.js
+++ 04-Front-End/08-Templating/02-Recipe-book/lib/index.js
@@ -1,6 +1,6 @@
 // TODO: Use the Meal DB API to search for recipes by ingredient
 
-// Todo: select the HTML elements you need
+// Todo: select the HTML elements you need here
 const searchInput = document.getElementById("search-input");
 const template = document.getElementById("recipe-template");
 const recipesContainer = document.getElementById("recipes-container");
@@ -11,49 +11,4 @@ const favourites = [];
 
 const mealApiUrl = "https://www.themealdb.com/api/json/v1/1/filter.php?i=";
 
-const addRecipeToFavourites = (event) => {
-  const idMeal = event.currentTarget.id;
-  const strMeal = event.currentTarget.parentElement.querySelector("h6").textContent;
-  const strMealThumb = event.currentTarget.parentElement.querySelector("img").src;
-
-  const newRecipeToAdd = { idMeal: idMeal, strMeal: strMeal, strMealThumb: strMealThumb };
-
-  favourites.push(newRecipeToAdd);
-  insertRecipes(favourites, favouritesContainer);
-}
-
-const addFavouriteListeners = () => {
-  const favouriteButtons = document.querySelectorAll(".fa-bookmark");
-  favouriteButtons.forEach((button) => {
-    button.addEventListener("click", (event) => {
-      addRecipeToFavourites(event);
-    });
-  });
-};
-
-const insertRecipes = (recipes, container) => {
-  if (recipes && recipes.length > 0) {
-    container.innerHTML = "";
-    recipes.forEach((result) => {
-      const clone = template.content.cloneNode(true);
-      clone.querySelector(".col-6").id = result.idMeal;
-      clone.querySelector(".fa-solid.fa-bookmark").id = result.idMeal;
-      clone.querySelector("img").src = result.strMealThumb;
-      clone.querySelector("h6").textContent = result.strMeal;
-      container.appendChild(clone);
-    });
-  }
-}
-
-searchInput.addEventListener("change", (event) => {
-  fetch(`${mealApiUrl}${event.target.value}`)
-    .then(response => response.json())
-    .then((data) => {
-      if (data.meals && data.meals.length > 0) {
-        insertRecipes(data.meals, recipesContainer);
-        addFavouriteListeners();
-      } else {
-        recipesContainer.innerHTML = "No results found! Try with another ingredient.";
-      }
-    })
-})
+// Todo: create a function to fetch the recipes from the API when the search input changes
