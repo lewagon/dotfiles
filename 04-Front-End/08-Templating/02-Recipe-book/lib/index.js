39667bc1598d75009eaa0b57f935444c0501988d
Thanh Tran <trandgthanh@gmail.com>
2023-03-03 16:03:24 +0100

    cleanup before merge

--- 04-Front-End/08-Templating/02-Recipe-book/lib/index.js
+++ 04-Front-End/08-Templating/02-Recipe-book/lib/index.js
@@ -1,14 +1,9 @@
-// TODO: Use the Meal DB API to search for recipes by ingredient
+// Todo: Use the Meal DB API to search for recipes by ingredient
 
 // Todo: select the HTML elements you need here
-const searchInput = document.getElementById("search-input");
-const template = document.getElementById("recipe-template");
-const recipesContainer = document.getElementById("recipes-container");
-const favouritesContainer = document.getElementById("favourites-container");
+// (such as search input, recipe-template, recipes-container, favourites-container)
 
-// Initialize the favourites array
-const favourites = [];
-
-const mealApiUrl = "https://www.themealdb.com/api/json/v1/1/filter.php?i=";
+// Todo: Initialize the favourites array
 
 // Todo: create a function to fetch the recipes from the API when the search input changes
+const mealApiUrl = "https://www.themealdb.com/api/json/v1/1/filter.php?i=";
