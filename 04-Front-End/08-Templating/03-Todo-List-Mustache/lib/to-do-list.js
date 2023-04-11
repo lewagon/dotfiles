3c8cc01fea7f1b869431f9c317911a85d441fe77
Alexander Dubovoy <ajdubovoy@gmail.com>
2023-04-11 17:13:08 -0400

    add mustache.js challenge

---
+++ 04-Front-End/08-Templating/03-Todo-List-Mustache/lib/to-do-list.js
@@ -0,0 +1,10 @@
+import Mustache from "mustachejs";
+
+const todos = [
+  { title: "Code a to-do list", done: false },
+  { title: "Eat breakfast", done: true },
+  { title: "Do some exercise", done: false },
+  { title: "Water the plants", done: true },
+];
+
+// TODO: Your code goes below:
