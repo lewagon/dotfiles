e7f7ccad7b53ca6508db8db52a41fb2dfa7ce7bf
Alexander Dubovoy <ajdubovoy@gmail.com>
2023-05-04 10:49:17 -0700

    Add comments that explicitly ask for return types in challenge 03-01-03

--- 03-AR-Database/01-DB-and-SQL/03-Interacting-with-code/lib/queries.rb
+++ 03-AR-Database/01-DB-and-SQL/03-Interacting-with-code/lib/queries.rb
@@ -1,21 +1,21 @@
 require 'sqlite3'
 
 def artist_count(db)
-  # TODO: use `db` to execute an SQL query against the database.
+  # TODO: use `db` to execute an SQL query against the database. Should return an integer of the number of artists.
 end
 
 def number_of_rows(db, table_name)
-  # TODO: count number of rows in table table_name
+  # TODO: count number of rows in table table_name. Should return an integer of the number of rows.
 end
 
 def sorted_artists(db)
-  # TODO: return array of artists' names sorted alphabetically
+  # TODO: return array of artists' names sorted alphabetically. Should return an array of strings sorted alphabetically.
 end
 
 def love_tracks(db)
-  # TODO: return array of love songs' names
+  # TODO: return array of love songs' names. Should return an array of strings (track names).
 end
 
 def long_tracks(db, min_length)
-  # TODO: return an array of tracks' names verifying: duration > min_length (minutes) sorted by length (ascending)
+  # TODO: return an array of tracks' names verifying: duration > min_length (minutes) sorted by length (ascending). Should return an array of strings.
 end
