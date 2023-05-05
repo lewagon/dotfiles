52ed15391a7e1114b58a4808b8d43cbff0544aec
Sébastien Saunier <seb@saunier.me>
2023-05-05 16:45:28 +0200

    Fixes #2332

--- 03-AR-Database/04-ActiveRecord-Advanced/03-Sinatra/lib/app.rb
+++ 03-AR-Database/04-ActiveRecord-Advanced/03-Sinatra/lib/app.rb
@@ -1,12 +1,6 @@
 require "sinatra"
 require "sinatra/reloader" if development?
 require "pry-byebug"
-require "better_errors"
-
-configure :development do
-  use BetterErrors::Middleware
-  BetterErrors.application_root = File.expand_path(__dir__)
-end
 
 require_relative "../config/application"
 
