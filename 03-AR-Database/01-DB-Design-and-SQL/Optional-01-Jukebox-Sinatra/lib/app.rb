c219e578258edc0c2ea2f25d3014f83403fabdd6
Anne de Joly <anne.dejoly@edhec.com>
2023-05-05 19:57:01 +0530

    Remove better_errors

--- 03-AR-Database/01-DB-Design-and-SQL/Optional-01-Jukebox-Sinatra/lib/app.rb
+++ 03-AR-Database/01-DB-Design-and-SQL/Optional-01-Jukebox-Sinatra/lib/app.rb
@@ -1,13 +1,7 @@
 require "sinatra"
 require "sinatra/reloader" if development?
 require "pry-byebug"
-require "better_errors"
 require "sqlite3"
-
-configure :development do
-  use BetterErrors::Middleware
-  BetterErrors.application_root = File.expand_path(__dir__)
-end
 
 DB = SQLite3::Database.new(File.join(File.dirname(__FILE__), "db/jukebox.sqlite"))
 
