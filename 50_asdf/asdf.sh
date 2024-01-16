5e8f0c2d65fec4597c25af890b8d03b98a0c7bd0
Miguel Hargreaves Pimenta <jmiguelpimenta@icloud.com>
2024-01-16 11:08:23 +0000

    build: add asdf python and alias plugins

--- 50_asdf/asdf.sh
+++ 50_asdf/asdf.sh
@@ -2,11 +2,14 @@
 
 sudo -v
 
-# Add nodejs plugin
 asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
-# Add yarn plugin
+
 asdf plugin-add yarn
-# Add rubby plugin
+
 asdf plugin add ruby https://github.com/asdf-vm/asdf-ruby.git
+
+asdf plugin add python
+
+asdf plugin add asdf-alias
 
 asdf install
