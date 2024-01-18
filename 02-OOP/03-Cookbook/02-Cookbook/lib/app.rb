40b2a661fcf19341a24dd27fbfff5daa91807877
Alexander Dubovoy <ajdubovoy@gmail.com>
2024-01-18 18:44:43 +0100

    Fix spec issue

--- 02-OOP/03-Cookbook/02-Cookbook/lib/app.rb
+++ 02-OOP/03-Cookbook/02-Cookbook/lib/app.rb
@@ -2,8 +2,7 @@ require_relative 'cookbook'
 require_relative 'controller'
 require_relative 'router'
 
-csv_file   = File.join(__dir__, 'recipes.csv')
-cookbook   = Cookbook.new(csv_file)
+cookbook   = Cookbook.new
 controller = Controller.new(cookbook)
 
 router = Router.new(controller)
