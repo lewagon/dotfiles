a3500e18a9ff0f106c7ef70b53eb3f06d5bb63b7
Miguel Hargreaves Pimenta <jmiguelpimenta@icloud.com>
2024-08-14 19:04:45 +0100

    refactor: change brew bin path

--- 20_homebrew/homebrew.sh
+++ 20_homebrew/homebrew.sh
@@ -6,19 +6,12 @@ echo "Installing Homebrew"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 echo "Done"
 
-echo "Add brew to PATH"
-# Set PATH, MANPATH, etc., for Homebrew.
-if [[ $(uname -m) == 'arm64' ]]; then
-  eval "$(/opt/homebrew/bin/brew shellenv)"
-fi
-echo "Done"
-
 echo "Installing Brew formulae"
-brew bundle --no-lock --file=20_homebrew/Brewfile
+/opt/homebrew/bin/brew bundle --no-lock --file=20_homebrew/Brewfile
 echo "Done"
 
 echo "Installing Brew casks"
-brew bundle --no-lock --file=20_homebrew/Caskfile
+/opt/homebrew/bin/brew bundle --no-lock --file=20_homebrew/Caskfile
 echo "Done"
 
 exit
--- 40_mackup/mackup.sh
+++ 40_mackup/mackup.sh
@@ -2,16 +2,12 @@
 
 sudo -v
 
-if [[ $(uname -m) == 'arm64' ]]; then
-  eval "$(/opt/homebrew/bin/brew shellenv)"
-fi
-
 ln -s $HOME"/Library/Mobile Documents/com~apple~CloudDocs/Mackup/.mackup.cfg" ~
 
 mackup restore
 
 # Import GPG keys
-chown -R $(whoami) ~/.gnupg/
-find ~/.gnupg -type f -exec chmod 600 {} \;
-find ~/.gnupg -type d -exec chmod 700 {} \;
-gpg --import-options restore --import ~/.gnupg/jmschp_private.gpg
+# chown -R $(whoami) ~/.gnupg/
+# find ~/.gnupg -type f -exec chmod 600 {} \;
+# find ~/.gnupg -type d -exec chmod 700 {} \;
+# gpg --import-options restore --import ~/.gnupg/jmschp_private.gpg
