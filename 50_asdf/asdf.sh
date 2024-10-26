e35abb067c852f353280bd4458e016434abeb7f3
Miguel Hargreaves Pimenta <jmiguelpimenta@icloud.com>
2024-10-26 20:01:55 +0100

    feat: improve asdf installation to latest versions

--- 50_asdf/asdf.sh
+++ 50_asdf/asdf.sh
@@ -2,14 +2,21 @@
 
 sudo -v
 
-asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
-
-asdf plugin add ruby https://github.com/asdf-vm/asdf-ruby.git
-
-asdf plugin add python
-
-asdf plugin add rust
-
 asdf plugin add alias
+asdf plugin add nodejs
+asdf plugin add python
+asdf plugin add rust
+asdf plugin add ruby
 
-asdf install
+asdf install $(asdf nodejs resolve lts --latest-available)
+asdf global nodejs $(asdf nodejs resolve lts --latest-available)
+
+asdf install python $(asdf latest python)
+asdf install python 2.7.18
+asdf global python $(asdf latest python) 2.7.18
+
+asdf install rust $(asdf latest rust)
+asdf global rust $(asdf latest rust)
+
+asdf install ruby $(asdf latest ruby)
+asdf global ruby $(asdf latest ruby)
