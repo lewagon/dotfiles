1a77664f3d642859aa5f63d708bdd61d4977c7b7
Miguel Hargreaves Pimenta <jmiguelpimenta@icloud.com>
2024-10-26 20:03:43 +0100

    chore: removed powerlevel10k and fonts

--- 30_oh-my-zsh/oh-my-zsh.sh
+++ 30_oh-my-zsh/oh-my-zsh.sh
@@ -7,18 +7,6 @@ echo "Installing Oh My Zsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 echo "Done"
 
-echo "Downloading fonts for Powerlevel10k"
-# Download and install the fonts need for Powerlevel10K terminal theme
-cd ~/Library/Fonts && { curl -L 'https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf' -o 'MesloLGS NF Regular.ttf' ; cd -; }
-cd ~/Library/Fonts && { curl -L 'https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf' -o 'MesloLGS NF Bold.ttf' ; cd -; }
-cd ~/Library/Fonts && { curl -L 'https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf' -o 'MesloLGS NF Italic.ttf' ; cd -; }
-cd ~/Library/Fonts && { curl -L 'https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf' -o 'MesloLGS NF Bold Italic.ttf' ; cd -; }
-echo "Done"
-
-echo "Installing Powerlevel10k theme"
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-echo "Done"
-
 echo "Installing zsh-users plugins"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
