a32078b59c0e41cf1cdc43aef175c12010b45f6a
Miguel Hargreaves Pimenta <jmiguelpimenta@icloud.com>
2024-10-27 12:10:08 +0000

    feat: add full XDG Base Directory Specification

--- prepare.sh
+++ prepare.sh
@@ -2,13 +2,21 @@
 
 sudo -v
 
-echo "Creating config folder"
-mkdir -p "${HOME}/.config"
+echo "Creating XDG Base Directory Specification"
+mkdir -p "$HOME/.cache"
+mkdir -p "$HOME/.config"
+mkdir -p "$HOME/.local/share"
+mkdir -p "$HOME/.local/state"
+mkdir -p "$HOME/.local/runtime"
+chmod 0700 "$HOME/.local/runtime"
 echo "Done"
 
 echo "Exporting XDG_CONFIG_HOME environement variable"
-touch ~/.zshenv
-echo export XDG_CONFIG_HOME=$HOME/.config >> ~/.zshenv
+touch "$HOME/.zshenv"
+echo export XDG_CACHE_HOME=$HOME/.cache >> "$HOME/.zshenv"
+echo export XDG_CONFIG_HOME=$HOME/.config >> "$HOME/.zshenv"
+echo export XDG_DATA_HOME=$HOME/.local/share >> "$HOME/.zshenv"
+echo export XDG_STATE_HOME=$HOME/.local/state >> "$HOME/.zshenv"
+echo export XDG_DATA_DIRS="/usr/local/share/:/usr/share/:/opt/homebrew/share" >> "$HOME/.zshenv"
+echo export XDG_RUNTIME_DIR="$HOME/.local/runtime" >> "$HOME/.zshenv"
 echo "Done"
-
-exit
