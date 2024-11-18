53df740e61e879d32d2bff096e9d962d97765842
Alexander Dubovoy <ajdubovoy@gmail.com>
2024-11-18 16:35:09 +0100

    Geocoder: Fix up order and function structure
    resolve https://github.com/lewagon/fullstack-challenges/issues/2740
    
    I relied mostly on our existing solution for this, so I feel fairly
    confident that this structure makes sense.
    
    resolve https://github.com/lewagon/fullstack-challenges/issues/2740

--- 04-Front-End/06-HTTP-and-API/03-Geocoder/lib/index.js
+++ 04-Front-End/06-HTTP-and-API/03-Geocoder/lib/index.js
@@ -1,24 +1,39 @@
-// TODO: Create a function to get the coordinates
-// from an address and display a map with a marker on it
-const showMap = (userInput) => {
-  // TODO: Construct the URL (with apiKey & userInput)
+const TOKEN = "your-token-here";
+
+const displayCoordinates = (longitude, latitude) => {
+  // TODO #7: Insert the coordinates into the DOM
+  // - Extract the coordinates from the parsed JSON response (lang, lat)
+  // - Display the coordinates in the element where the coordinates will be displayed
+};
+
+const injectMap = (lng, lat) => {
+  // TODO #6: Create the map
+  // - Create a map using the Mapbox API
+  // - Add a marker to the map at the coordinates
+};
+
+const showMapAndCoordinates = (event) => {
+  event.preventDefault();
+  // TODO #3 Get the user input
+  const coordinates = "";
+  // TODO #4: Construct the URL (with apiKey & userInput)
   // and make the fetch request to the mapbox API
-  const url = `...`;
+  const url = ``;
 
   fetch(url)
     .then(response => response.json())
     .then((data) => {
       console.log(data);
-      // TODO: Insert the info into the DOM
-      // - Extract the coordinates from the parsed JSON response (lang, lat)
-      // - Display the coordinates in the element where the coordinates will be displayed
-      // - Create a map using the Mapbox API and the coordinates
-      // - Add a marker to the map at the coordinates
+      // TODO #5: Extract the coordinates from the parsed JSON response (longitude, latitude)
+      // Use these coordinates to call the displayCoordinates and injectMap functions
     });
 };
-//
-// TODO: Select the form element
-// TODO: Add event listener to the form that:
+
+// ### ### ### ### ###
+// ### START HERE! ###
+// ### ### ### ### ###
+// TODO #1: Select the form element
+// TODO #2: Add event listener to the form that:
 // - Prevents the default form submission behavior
 // - Get the user input
-// - Calls the showMap function with the user input as an argument
+// - Calls the showMapAndCoordinates function with the event (from the event listener) as an argument
